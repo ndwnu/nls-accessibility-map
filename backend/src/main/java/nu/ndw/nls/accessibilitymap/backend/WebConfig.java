1a5605e7970344fa7c4d374528a164237d7d9564
m durieux <167181728+ndwlocatieservices@users.noreply.github.com>
2023-10-31 16:42:59 +0100

    #62203 create api specification and basic it.

---
+++ backend/src/main/java/nu/ndw/nls/accessibilitymap/backend/WebConfig.java
@@ -0,0 +1,28 @@
+package nu.ndw.nls.accessibilitymap.backend;
+
+import java.util.List;
+import nu.ndw.nls.accessibilitymap.backend.generated.model.v1.VehicleTypeJson;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.format.FormatterRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    /*
+     * Use of marker interfaces fixes runtime errors when using spring converters as lambda expressions see:
+     * https://stackoverflow.com/questions/25711858/spring-cant-determine-generic-types-when-lambda-expression-is-used-instead-of-a
+     *
+     */
+    private interface VehicleTypeJsonConverter extends Converter<String, VehicleTypeJson> {
+
+    }
+
+    @Override
+    public void addFormatters(FormatterRegistry registry) {
+        List.of(
+                        (VehicleTypeJsonConverter) VehicleTypeJson::fromValue)
+                .forEach(registry::addConverter);
+    }
+}
