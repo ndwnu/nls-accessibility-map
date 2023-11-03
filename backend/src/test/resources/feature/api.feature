d634a7f90329fa75fc4b725eceea730f972f2b72
NDW Locatie Services <167181728+ndwlocatieservices@users.noreply.github.com>
2023-11-03 11:26:29 +0100

    #62357 Parameterize traffic signs URL

--- backend/src/test/resources/feature/api.feature
+++ backend/src/test/resources/feature/api.feature
@@ -6,20 +6,20 @@ Feature: API operations
     * def invalidToken = karate.properties['auth.token.nls-accessibility-map-api-invalid-token-test']
     * configure headers = call read('classpath:headers.js') { token: #(validToken)}
 
-  Scenario: accessibility map  should return 200
+  Scenario: accessibility map should return 200
     Given path '/v1/municipalities/344'
     And param vehicleType = 'Car'
     And method GET
     Then status 200
 
-  Scenario: accessibility without token should return 401
+  Scenario: accessibility map without token should return 401
     * configure headers = null
     Given path '/v1/municipalities/344'
     And param vehicleType = 'Car'
     And method GET
     Then status 401
 
-  Scenario: accessibility with invalid token should return 403
+  Scenario: accessibility map with invalid token should return 403
     * configure headers = call read('classpath:headers.js') { token: #(invalidToken)}
     Given path '/v1/municipalities/344'
     And param vehicleType = 'Car'
--- jobs/src/main/java/nu/ndw/nls/accessibilitymap/jobs/trafficsigns/services/TrafficSignService.java
+++ jobs/src/main/java/nu/ndw/nls/accessibilitymap/jobs/trafficsigns/services/TrafficSignService.java
@@ -2,9 +2,7 @@ package nu.ndw.nls.accessibilitymap.jobs.trafficsigns.services;
 
 import java.util.Comparator;
 import java.util.stream.Stream;
-import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
-import lombok.extern.slf4j.Slf4j;
 import nu.ndw.nls.accessibilitymap.jobs.trafficsigns.dtos.TrafficSignJsonDtoV3;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpHeaders;
@@ -13,18 +11,20 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 
 @Service
-@RequiredArgsConstructor
 public class TrafficSignService {
 
-    @Value("${itTest}")
-    private boolean itTest;
     private final WebClient webClient;
+    private final String currentStateUri;
+
+    public TrafficSignService(WebClient webClient, @Value("${traffic-sign.current-state-uri}") String currentStateUri) {
+        this.webClient = webClient;
+        this.currentStateUri = currentStateUri;
+    }
 
     @SneakyThrows
     public Stream<TrafficSignJsonDtoV3> getTrafficSigns() {
-        String url = itTest ? "/current-state?status=PLACED&town-code=GM0307" : "/current-state?status=PLACED";
         return webClient.get()
-                .uri(url)
+                .uri(currentStateUri)
                 .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
                 .retrieve()
                 .bodyToFlux(TrafficSignJsonDtoV3.class)
