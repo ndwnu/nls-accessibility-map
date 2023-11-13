197a88633bf18d3d793ddaecc9a51e88f37ae866
Bas Duineveld <167181728+ndwlocatieservices@users.noreply.github.com>
2023-11-13 15:37:02 +0100

    #62425 Store current-state timestamp in routing network

--- jobs/src/test/java/nu/ndw/nls/accessibilitymap/jobs/services/AccessibilityNetworkServiceTest.java
+++ jobs/src/test/java/nu/ndw/nls/accessibilitymap/jobs/services/AccessibilityNetworkServiceTest.java
@@ -8,9 +8,11 @@ import static org.mockito.Mockito.when;
 
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.time.Instant;
 import java.util.Iterator;
 import java.util.List;
 import lombok.SneakyThrows;
+import nu.ndw.nls.accessibilitymap.jobs.services.AccessibilityLinkService.AccessibilityLinkResponse;
 import nu.ndw.nls.db.nwb.jooq.services.NwbVersionCrudService;
 import nu.ndw.nls.routingmapmatcher.domain.model.Link;
 import nu.ndw.nls.routingmapmatcher.domain.model.RoutingNetwork;
@@ -28,6 +30,7 @@ class AccessibilityNetworkServiceTest {
 
     private static final String GRAPHHOPPER_DIR = "/tmp/graphhopper";
     private static final String NETWORK_NAME = "accessibility_latest";
+    private static final Instant DATA_DATE = Instant.parse("2023-11-07T15:37:23Z");
 
     @Mock
     private AccessibilityGraphHopperNetworkService accessibilityGraphHopperNetworkService;
@@ -58,7 +61,7 @@ class AccessibilityNetworkServiceTest {
     @Test
     void storeLatestNetworkOnDisk_ok() {
         when(nwbVersionService.findLatestVersionId()).thenReturn(20231001);
-        when(accessibilityLinkService.getLinks(20231001)).thenReturn(links);
+        when(accessibilityLinkService.getLinks(20231001)).thenReturn(new AccessibilityLinkResponse(links, DATA_DATE));
         when(links.iterator()).thenReturn(linkIterator);
 
         accessibilityNetworkService.storeLatestNetworkOnDisk();
@@ -68,6 +71,7 @@ class AccessibilityNetworkServiceTest {
         RoutingNetwork routingNetwork = routingNetworkArgumentCaptor.getValue();
         assertEquals(NETWORK_NAME, routingNetwork.getNetworkNameAndVersion());
         assertEquals(linkIterator, routingNetwork.getLinkSupplier().get());
+        assertEquals(DATA_DATE, routingNetwork.getDataDate());
         assertTrue(Files.exists(Path.of(GRAPHHOPPER_DIR, NETWORK_NAME)));
     }
 }
