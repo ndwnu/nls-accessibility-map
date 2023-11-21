1b4760e8443f57b830245f095b4e4245cf0679da
Bas Duineveld <167181728+ndwlocatieservices@users.noreply.github.com>
2023-11-21 20:26:38 +0100

    #63349 Sort road section IDs to prevent flaky tests

--- backend/src/test/java/nu/ndw/nls/accessibilitymap/backend/mappers/ResponseMapperTest.java
+++ backend/src/test/java/nu/ndw/nls/accessibilitymap/backend/mappers/ResponseMapperTest.java
@@ -11,66 +11,77 @@ import org.junit.jupiter.api.Test;
 class ResponseMapperTest {
 
     private static final int ID = 1;
+    private static final int ID_2 = 2;
+
     private ResponseMapper responseMapper;
 
     @BeforeEach
-    void setup() {
+    void setUp() {
         responseMapper = new ResponseMapper();
     }
 
     @Test
-    void mapToRoadSectionsJson_two_road_sections_ok() {
-
+    void mapToRoadSectionsJson_ok_twoDirections() {
         IsochroneMatch isochroneMatchReversed = IsochroneMatch
                 .builder()
                 .matchedLinkId(ID)
                 .reversed(true)
                 .build();
-
         IsochroneMatch isochroneMatch = IsochroneMatch
                 .builder()
                 .matchedLinkId(ID)
                 .build();
-
         Set<IsochroneMatch> matches = Set.of(isochroneMatchReversed, isochroneMatch);
 
         RoadSectionsJson result = responseMapper.mapToRoadSectionsJson(matches);
-        assertThat(result.getInaccessibleRoadSections()).hasSize(1);
 
+        assertThat(result.getInaccessibleRoadSections()).hasSize(1);
         assertThat(result.getInaccessibleRoadSections().get(0).getRoadSectionId())
                 .isEqualTo(ID);
-
         assertThat(result.getInaccessibleRoadSections().get(0).getBackwardAccessible())
                 .isEqualTo(true);
-
         assertThat(result.getInaccessibleRoadSections().get(0).getForwardAccessible())
                 .isEqualTo(true);
-
     }
 
-
     @Test
-    void mapToRoadSectionsJson_one_road_section_ok() {
-
+    void mapToRoadSectionsJson_ok_oneDirection() {
         IsochroneMatch isochroneMatchReversed = IsochroneMatch
                 .builder()
                 .matchedLinkId(ID)
                 .reversed(true)
                 .build();
-
         Set<IsochroneMatch> matches = Set.of(isochroneMatchReversed);
 
         RoadSectionsJson result = responseMapper.mapToRoadSectionsJson(matches);
-        assertThat(result.getInaccessibleRoadSections()).hasSize(1);
 
+        assertThat(result.getInaccessibleRoadSections()).hasSize(1);
         assertThat(result.getInaccessibleRoadSections().get(0).getRoadSectionId())
                 .isEqualTo(ID);
-
         assertThat(result.getInaccessibleRoadSections().get(0).getBackwardAccessible())
                 .isEqualTo(true);
-
         assertThat(result.getInaccessibleRoadSections().get(0).getForwardAccessible())
                 .isEqualTo(false);
+    }
 
+    @Test
+    void mapToRoadSectionsJson_ok_sort() {
+        IsochroneMatch isochroneMatch1 = IsochroneMatch
+                .builder()
+                .matchedLinkId(ID)
+                .build();
+        IsochroneMatch isochroneMatch2 = IsochroneMatch
+                .builder()
+                .matchedLinkId(ID_2)
+                .build();
+        Set<IsochroneMatch> matches = Set.of(isochroneMatch2, isochroneMatch1);
+
+        RoadSectionsJson result = responseMapper.mapToRoadSectionsJson(matches);
+
+        assertThat(result.getInaccessibleRoadSections()).hasSize(2);
+        assertThat(result.getInaccessibleRoadSections().get(0).getRoadSectionId())
+                .isEqualTo(ID);
+        assertThat(result.getInaccessibleRoadSections().get(1).getRoadSectionId())
+                .isEqualTo(ID_2);
     }
 }
