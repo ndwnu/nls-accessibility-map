8d116c2ebcc97b4f085e6c9857b446a7053fba7f
NDW Locatie Services <167181728+ndwlocatieservices@users.noreply.github.com>
2023-11-24 14:49:33 +0100

    #63309 Add endpoint to fetch all municipalities as geojson

---
+++ backend/src/main/java/nu/ndw/nls/accessibilitymap/backend/controllers/MunicipalitiesApiDelegateImpl.java
@@ -0,0 +1,24 @@
+package nu.ndw.nls.accessibilitymap.backend.controllers;
+
+import lombok.RequiredArgsConstructor;
+import nu.ndw.nls.accessibilitymap.backend.config.MunicipalityProperties;
+import nu.ndw.nls.accessibilitymap.backend.generated.api.v1.MunicipalitiesApiDelegate;
+import nu.ndw.nls.accessibilitymap.backend.generated.model.v1.FeatureCollectionJson;
+import nu.ndw.nls.accessibilitymap.backend.mappers.MunicipalityMapper;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class MunicipalitiesApiDelegateImpl implements MunicipalitiesApiDelegate {
+
+    private final MunicipalityMapper municipalityMapper;
+    private final MunicipalityProperties municipalityProperties;
+
+    @Override
+    public ResponseEntity<FeatureCollectionJson> getMunicipalities() {
+        return ResponseEntity.ok(municipalityMapper.mapToMunicipalitiesToGeoJSON(
+                municipalityProperties.getMunicipalities().values().stream().toList()));
+    }
+
+}
---
+++ backend/src/test/java/nu/ndw/nls/accessibilitymap/backend/mappers/MunicipalityMapperTest.java
@@ -0,0 +1,35 @@
+package nu.ndw.nls.accessibilitymap.backend.mappers;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
+import nu.ndw.nls.accessibilitymap.backend.generated.model.v1.PointJson;
+import nu.ndw.nls.accessibilitymap.backend.services.TestHelper;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class MunicipalityMapperTest {
+
+    private MunicipalityMapper municipalityMapper;
+
+    @BeforeEach
+    void setup() {
+        municipalityMapper = new MunicipalityMapper();
+    }
+
+    @Test
+    void mapMunicipality() {
+        var geoJSON = municipalityMapper.mapToMunicipalitiesToGeoJSON(List.of(TestHelper.MUNICIPALITY));
+        assertThat(geoJSON.getType().getValue()).isEqualTo("FeatureCollection");
+        assertThat(geoJSON.getFeatures()).hasSize(1);
+        assertThat(geoJSON.getFeatures().get(0).getId()).isEqualTo(TestHelper.MUNICIPALITY_ID);
+        var geometry = geoJSON.getFeatures().get(0).getGeometry();
+        assertThat(geometry.getType().getValue()).isEqualTo("Point");
+        assertThat(geometry).isInstanceOf(PointJson.class);
+        var coordinates = ((PointJson) geometry).getCoordinates();
+        assertThat(coordinates.get(0)).isEqualTo(TestHelper.MUNICIPALITY.getStartPoint().getCoordinates()[0].x);
+        assertThat(coordinates.get(1)).isEqualTo(TestHelper.MUNICIPALITY.getStartPoint().getCoordinates()[0].y);
+        var properties = geoJSON.getFeatures().get(0).getProperties();
+        assertThat(properties).isNotNull();
+    }
+}
