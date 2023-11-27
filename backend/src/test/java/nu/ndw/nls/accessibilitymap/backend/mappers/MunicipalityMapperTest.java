02496260e6cc1d5ef36099bb196e33c12e77191f
NDW Locatie Services <167181728+ndwlocatieservices@users.noreply.github.com>
2023-11-27 11:54:06 +0100

    #63309 Fix review comments

--- backend/src/test/java/nu/ndw/nls/accessibilitymap/backend/mappers/MunicipalityMapperTest.java
+++ backend/src/test/java/nu/ndw/nls/accessibilitymap/backend/mappers/MunicipalityMapperTest.java
@@ -1,10 +1,14 @@
 package nu.ndw.nls.accessibilitymap.backend.mappers;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static nu.ndw.nls.accessibilitymap.backend.services.TestHelper.MUNICIPALITY;
 
 import java.util.List;
+import nu.ndw.nls.accessibilitymap.backend.generated.model.v1.FeatureCollectionJson;
+import nu.ndw.nls.accessibilitymap.backend.generated.model.v1.FeatureCollectionJson.TypeEnum;
+import nu.ndw.nls.accessibilitymap.backend.generated.model.v1.FeatureJson;
+import nu.ndw.nls.accessibilitymap.backend.generated.model.v1.GeometryJson;
+import nu.ndw.nls.accessibilitymap.backend.generated.model.v1.MunicipalityPropertiesJson;
 import nu.ndw.nls.accessibilitymap.backend.generated.model.v1.PointJson;
-import nu.ndw.nls.accessibilitymap.backend.services.TestHelper;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -19,17 +23,14 @@ class MunicipalityMapperTest {
 
     @Test
     void mapToMunicipalitiesToGeoJSON_ok() {
-        var geoJSON = municipalityMapper.mapToMunicipalitiesToGeoJSON(List.of(TestHelper.MUNICIPALITY));
-        assertThat(geoJSON.getType().getValue()).isEqualTo("FeatureCollection");
-        assertThat(geoJSON.getFeatures()).hasSize(1);
-        assertThat(geoJSON.getFeatures().get(0).getId()).isEqualTo(TestHelper.MUNICIPALITY_ID);
-        var geometry = geoJSON.getFeatures().get(0).getGeometry();
-        assertThat(geometry.getType().getValue()).isEqualTo("Point");
-        assertThat(geometry).isInstanceOf(PointJson.class);
-        var coordinates = ((PointJson) geometry).getCoordinates();
-        assertThat(coordinates.get(0)).isEqualTo(TestHelper.MUNICIPALITY.getStartPoint().getCoordinates()[0].x);
-        assertThat(coordinates.get(1)).isEqualTo(TestHelper.MUNICIPALITY.getStartPoint().getCoordinates()[0].y);
-        var properties = geoJSON.getFeatures().get(0).getProperties();
-        assertThat(properties).isNotNull();
+        var expectedResult = new FeatureCollectionJson(TypeEnum.FEATURECOLLECTION,
+                List.of(new FeatureJson(FeatureJson.TypeEnum.FEATURE, MUNICIPALITY.getMunicipalityId(),
+                        new PointJson(GeometryJson.TypeEnum.POINT).coordinates(
+                                List.of(MUNICIPALITY.getStartPoint().getX(),
+                                        MUNICIPALITY.getStartPoint().getY()))).properties(
+                        new MunicipalityPropertiesJson(MUNICIPALITY.getName(),
+                                (int) MUNICIPALITY.getSearchDistanceInMetres()))));
+        var geoJSON = municipalityMapper.mapToMunicipalitiesToGeoJSON(List.of(MUNICIPALITY));
+        assert (geoJSON.equals(expectedResult));
     }
 }
