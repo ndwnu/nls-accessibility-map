e9247c1b3f85cee11cdabebd34172fd2ab050966
Eelco van Dijk <167181728+ndwlocatieservices@users.noreply.github.com>
2024-04-18 09:17:28 +0000

    Merged PR 48169: 66271: Tracing
    #66271: Tracing

--- jobs/src/main/java/nu/ndw/nls/accessibilitymap/jobs/AccessibilityMapJobConfiguration.java
+++ jobs/src/main/java/nu/ndw/nls/accessibilitymap/jobs/AccessibilityMapJobConfiguration.java
@@ -4,6 +4,7 @@ import nu.ndw.nls.accessibilitymap.shared.SharedConfiguration;
 import nu.ndw.nls.accessibilitymap.trafficsignclient.TrafficSignConfiguration;
 import nu.ndw.nls.events.NlsEventType;
 import nu.ndw.nls.routingmapmatcher.RoutingMapMatcherConfiguration;
+import nu.ndw.nls.springboot.datadog.DatadogConfiguration;
 import nu.ndw.nls.springboot.messaging.MessagingConfig;
 import nu.ndw.nls.springboot.messaging.properties.validation.MessagingRequiredConfiguration;
 import org.springframework.context.annotation.Configuration;
@@ -11,7 +12,7 @@ import org.springframework.context.annotation.Import;
 
 @Configuration
 @Import({TrafficSignConfiguration.class, RoutingMapMatcherConfiguration.class, MessagingConfig.class,
-        MessagingConfig.class, SharedConfiguration.class})
+        MessagingConfig.class, SharedConfiguration.class, DatadogConfiguration.class})
 @MessagingRequiredConfiguration(receive = {}, publish = {NlsEventType.ACCESSIBILITY_ROUTING_NETWORK_UPDATED})
 public class AccessibilityMapJobConfiguration {
 
