efdd42e79dbd9c359369d28e4edaa5ef09d29484
NDW Locatie Services <167181728+ndwlocatieservices@users.noreply.github.com>
2024-06-17 14:21:22 +0200

    #73980 First simple GeoJSON implementation

--- backend/src/main/java/nu/ndw/nls/accessibilitymap/backend/controllers/AccessibilityMapApiDelegateImpl.java
+++ backend/src/main/java/nu/ndw/nls/accessibilitymap/backend/controllers/AccessibilityMapApiDelegateImpl.java
@@ -9,15 +9,17 @@ import nu.ndw.nls.accessibilitymap.backend.exceptions.PointMatchingRoadSectionNo
 import nu.ndw.nls.accessibilitymap.backend.exceptions.VehicleWeightRequiredException;
 import nu.ndw.nls.accessibilitymap.backend.generated.api.v1.AccessibilityMapApiDelegate;
 import nu.ndw.nls.accessibilitymap.backend.generated.model.v1.AccessibilityMapResponseJson;
+import nu.ndw.nls.accessibilitymap.backend.generated.model.v1.RoadSectionFeatureCollectionJson;
 import nu.ndw.nls.accessibilitymap.backend.generated.model.v1.VehicleTypeJson;
 import nu.ndw.nls.accessibilitymap.backend.mappers.AccessibilityResponseMapper;
 import nu.ndw.nls.accessibilitymap.backend.mappers.PointMapper;
 import nu.ndw.nls.accessibilitymap.backend.mappers.RequestMapper;
+import nu.ndw.nls.accessibilitymap.backend.mappers.RoadSectionFeatureCollectionMapper;
 import nu.ndw.nls.accessibilitymap.backend.model.RoadSection;
+import nu.ndw.nls.accessibilitymap.backend.model.VehicleProperties;
 import nu.ndw.nls.accessibilitymap.backend.services.AccessibilityMapService;
 import nu.ndw.nls.accessibilitymap.backend.services.PointMatchService;
 import nu.ndw.nls.accessibilitymap.backend.validators.PointValidator;
-import nu.ndw.nls.accessibilitymap.backend.model.VehicleProperties;
 import nu.ndw.nls.routingmapmatcher.model.singlepoint.SinglePointMatch.CandidateMatch;
 import org.locationtech.jts.geom.Point;
 import org.springframework.http.ResponseEntity;
@@ -30,6 +32,7 @@ public class AccessibilityMapApiDelegateImpl implements AccessibilityMapApiDeleg
 
     private final RequestMapper requestMapper;
     private final AccessibilityResponseMapper accessibilityMapResultMapper;
+    private final RoadSectionFeatureCollectionMapper roadSectionFeatureCollectionMapper;
     private final PointMapper pointMapper;
 
     private final AccessibilityMapService accessibilityMapService;
@@ -41,30 +44,51 @@ public class AccessibilityMapApiDelegateImpl implements AccessibilityMapApiDeleg
             VehicleTypeJson vehicleType, Float vehicleLength, Float vehicleWidth, Float vehicleHeight,
             Float vehicleWeight, Float vehicleAxleLoad, Boolean vehicleHasTrailer, Double latitude, Double longitude) {
         checkWeightConstraint(vehicleType, vehicleWeight);
-        pointValidator.validateConsistentValues(latitude, longitude);
+        CandidateMatch startPointMatch = matchStartPoint(latitude, longitude);
+        Integer requestedRoadSectionId = startPointMatch != null ? startPointMatch.getMatchedLinkId() : null;
 
+        VehicleArguments requestArguments = new VehicleArguments(vehicleType, vehicleLength, vehicleWidth,
+                vehicleHeight, vehicleWeight, vehicleAxleLoad, vehicleHasTrailer == Boolean.TRUE);
+        SortedMap<Integer, RoadSection> idToRoadSection = getAccessibility(municipalityId, requestArguments);
+
+        return ResponseEntity.ok(accessibilityMapResultMapper.map(idToRoadSection, requestedRoadSectionId));
+    }
+
+    @Override
+    public ResponseEntity<RoadSectionFeatureCollectionJson> getRoadSections(String municipalityId,
+            VehicleTypeJson vehicleType, Float vehicleLength, Float vehicleWidth, Float vehicleHeight,
+            Float vehicleWeight, Float vehicleAxleLoad, Boolean vehicleHasTrailer, Double latitude, Double longitude) {
+        checkWeightConstraint(vehicleType, vehicleWeight);
+        CandidateMatch startPointMatch = matchStartPoint(latitude, longitude);
+
+        VehicleArguments requestArguments = new VehicleArguments(vehicleType, vehicleLength, vehicleWidth,
+                vehicleHeight, vehicleWeight, vehicleAxleLoad, vehicleHasTrailer == Boolean.TRUE);
+        SortedMap<Integer, RoadSection> idToRoadSection = getAccessibility(municipalityId, requestArguments);
+
+        return ResponseEntity.ok(roadSectionFeatureCollectionMapper.map(idToRoadSection, startPointMatch));
+    }
+
+    private CandidateMatch matchStartPoint(Double latitude, Double longitude) {
+        pointValidator.validateConsistentValues(latitude, longitude);
         Point startPoint = pointMapper.mapCoordinateAllowNulls(latitude, longitude);
-        Integer requestedRoadSectionId = null;
 
         if (Objects.nonNull(startPoint)) {
             CandidateMatch candidateMatch = pointMatchService.match(startPoint).orElseThrow(() ->
                     new PointMatchingRoadSectionNotFoundException("Could not find road section by latitude: " +
                             latitude + " longitude: " + longitude));
-            requestedRoadSectionId = candidateMatch.getMatchedLinkId();
 
-            log.debug("Found road section id: {} by latitude: {}, longitude {}", requestedRoadSectionId, latitude,
-                    longitude);
+            log.debug("Found road section id: {} by latitude: {}, longitude {}", candidateMatch.getMatchedLinkId(),
+                    latitude, longitude);
+
+            return candidateMatch;
         }
-
-        VehicleArguments requestArguments = new VehicleArguments(vehicleType, vehicleLength, vehicleWidth,
-                vehicleHeight, vehicleWeight, vehicleAxleLoad, vehicleHasTrailer == Boolean.TRUE);
-        VehicleProperties vehicleProperties = requestMapper.mapToVehicleProperties(requestArguments);
-        SortedMap<Integer, RoadSection> idToRoadSection = accessibilityMapService
-                .determineAccessibilityByRoadSection(vehicleProperties, municipalityId);
-
-        return ResponseEntity.ok(accessibilityMapResultMapper.map(idToRoadSection, requestedRoadSectionId));
+        return null;
     }
 
+    private SortedMap<Integer, RoadSection> getAccessibility(String municipalityId, VehicleArguments requestArguments) {
+        VehicleProperties vehicleProperties = requestMapper.mapToVehicleProperties(requestArguments);
+        return accessibilityMapService.determineAccessibilityByRoadSection(vehicleProperties, municipalityId);
+    }
 
     private static void checkWeightConstraint(VehicleTypeJson vehicleType, Float vehicleWeight) {
         if (VehicleTypeJson.COMMERCIAL_VEHICLE == vehicleType && vehicleWeight == null) {
