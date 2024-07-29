1a182611525db085f25e530d052b4e736bd74902
NDW Locatie Services <167181728+ndwlocatieservices@users.noreply.github.com>
2024-07-29 16:22:29 +0200

    #76201 Fixing more coverage

--- shared/src/test/java/nu/ndw/nls/accessibilitymap/shared/network/services/NetworkMetaDataServiceTest.java
+++ shared/src/test/java/nu/ndw/nls/accessibilitymap/shared/network/services/NetworkMetaDataServiceTest.java
@@ -1,6 +1,8 @@
 package nu.ndw.nls.accessibilitymap.shared.network.services;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.when;
 
 import java.io.File;
@@ -21,6 +23,8 @@ class NetworkMetaDataServiceTest {
 
     private static final String EXPECT_FILE_PATH_STRING = "src/test/resources/network-meta-data-expected-value.json";
     private static final File EXPECTED_CONTENT = new File(EXPECT_FILE_PATH_STRING);
+    private static final String FILE_DOES_NOT_EXIST = "file-does-not-exist";
+    private static final String TMP_DIRECTORY_IS_NOT_A_WRITEABLE_FILE = "tmp-directory-is-not-a-writeable-file";
     @Mock
     private GraphHopperConfiguration graphHopperConfiguration;
 
@@ -36,12 +40,38 @@ class NetworkMetaDataServiceTest {
 
     @Test
     @SneakyThrows
-    void saveMetaData() {
+    void loadMetaData_fail_ioexception() {
+        when(graphHopperConfiguration.getMetaDataPath()).thenReturn(Path.of(FILE_DOES_NOT_EXIST));
+        IllegalStateException illegalStateException = assertThrows(IllegalStateException.class,
+                () -> networkMetaDataService.loadMetaData());
+
+        assertEquals("Could not load meta-data from file path: file-does-not-exist",
+                illegalStateException.getMessage());
+    }
+
+    @Test
+    @SneakyThrows
+    void saveMetaData_ok() {
         Path tempFile = Files.createTempFile("meta-data", ".json");
         when(graphHopperConfiguration.getMetaDataPath()).thenReturn(tempFile);
 
         networkMetaDataService.saveMetaData(new AccessibilityGraphhopperMetaData(20241231));
         assertTrue(Files.exists(tempFile));
         FileUtils.contentEquals(EXPECTED_CONTENT, tempFile.toFile());
+    }
+
+    @Test
+    @SneakyThrows
+    void saveMetaData_fail_ioexception() {
+
+        Path tempDirectory = Files.createTempDirectory(TMP_DIRECTORY_IS_NOT_A_WRITEABLE_FILE);
+        when(graphHopperConfiguration.getMetaDataPath())
+                .thenReturn(tempDirectory);
+
+        IllegalStateException illegalStateException = assertThrows(IllegalStateException.class,
+                () -> networkMetaDataService.saveMetaData(new AccessibilityGraphhopperMetaData(20241231)));
+
+        assertEquals("Could not write meta-data to file path: " + tempDirectory,
+                illegalStateException.getMessage());
     }
 }
