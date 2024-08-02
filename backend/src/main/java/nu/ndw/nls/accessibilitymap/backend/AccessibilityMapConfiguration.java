98133e11bd0a8d741a273e337df8d1cdb08ac07d
Guido Senff <167181728+ndwlocatieservices@users.noreply.github.com>
2024-08-02 17:30:17 +0200

    #76590 Fixes

--- backend/src/main/java/nu/ndw/nls/accessibilitymap/backend/AccessibilityMapConfiguration.java
+++ backend/src/main/java/nu/ndw/nls/accessibilitymap/backend/AccessibilityMapConfiguration.java
@@ -6,8 +6,10 @@ import nu.ndw.nls.accessibilitymap.shared.properties.GraphHopperProperties;
 import nu.ndw.nls.geometry.GeometryConfiguration;
 import nu.ndw.nls.springboot.datadog.DatadogConfiguration;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
+@Configuration
 @Import({DatadogConfiguration.class, GeometryConfiguration.class, SharedConfiguration.class,
         AccessibilityConfiguration.class})
 @EnableConfigurationProperties(GraphHopperProperties.class)
