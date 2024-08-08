20fd98104e8fd0eec6d27d55f4094e75949064c2
Guido Senff <167181728+ndwlocatieservices@users.noreply.github.com>
2024-08-08 17:50:16 +0200

    #76610 Added message published coverage for the graphhopper network job

--- jobs/graphhopper/src/test/java/nu/ndw/nls/accessibilitymap/jobs/graphhopper/TestConfig.java
+++ jobs/graphhopper/src/test/java/nu/ndw/nls/accessibilitymap/jobs/graphhopper/TestConfig.java
@@ -1,10 +1,13 @@
 package nu.ndw.nls.accessibilitymap.jobs.graphhopper;
 
+import nu.ndw.nls.springboot.messaging.MessagingConfig;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 
 @Configuration
 @ComponentScan
+@Import(MessagingConfig.class)
 public class TestConfig {
 
 }
