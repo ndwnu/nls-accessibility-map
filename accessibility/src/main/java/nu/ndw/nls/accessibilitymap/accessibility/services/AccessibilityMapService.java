2dd43d463155e6ba59e26d1269903398420fc9f8
NDW Locatie Services <167181728+ndwlocatieservices@users.noreply.github.com>
2024-08-11 13:26:41 +0200

    #76610 Added type to allow different ways of determining the accessibility end results

--- accessibility/src/main/java/nu/ndw/nls/accessibilitymap/accessibility/services/AccessibilityMapService.java
+++ accessibility/src/main/java/nu/ndw/nls/accessibilitymap/accessibility/services/AccessibilityMapService.java
@@ -32,30 +32,88 @@ public class AccessibilityMapService {
     private final AccessibleRoadsService accessibleRoadsService;
     private final AccessibleRoadSectionsService accessibleRoadSectionsService;
 
+    public enum ResultType {
+        // Uses isochrone for determining base accessibility for the NWB road sections and then also for when
+        // the additional road sign restrictions are applied and finally subtracts them to find the difference
+        // This is useful for when you want to know how traffic signs are adding additional restrictions. In this mode,
+        // you will not find road sections that have no (car accessible) roads connecting them, because they are not
+        // accessible in the base isochrone response so there is no difference between the base accessibility and the
+        // one generated with added traffic sign restrictions
+        DIFFERENCE_OF_ADDED_RESTRICTIONS,
+        // Includes all NWB road sections to determine the accessibility and then uses isochrone to determine how
+        // traffic signs are adding additional restrictions. The result is a map that indicates which road sections
+        // you cannot access starting from the start point
+        EFFECTIVE_ACCESSIBILITY;
+    }
 
     public SortedMap<Integer, RoadSection> determineAccessibilityByRoadSection(VehicleProperties vehicleProperties,
-            Point startPoint, double searchDistanceInMeters) {
+            Point startPoint, double searchDistanceInMeters, ResultType resultType) {
         AccessibilityMap accessibilityMap = accessibilityMapFactory.createMapMatcher(networkGraphHopper);
 
         List<IsochroneMatch> accessibleRoadsWithRestrictions = accessibleRoadsService
                 .getVehicleAccessibleRoads(accessibilityMap, vehicleProperties, startPoint, searchDistanceInMeters);
 
-        return determineDifference(accessibleRoadsWithRestrictions, accessibleRoadSectionsService::getRoadSections);
+        if (resultType == ResultType.EFFECTIVE_ACCESSIBILITY) {
+            return determineUnion(accessibleRoadsWithRestrictions, accessibleRoadSectionsService::getRoadSections);
+        } else {
+            List<IsochroneMatch> baseAccessibleIsochrone = accessibleRoadsService.getBaseAccessibleRoads(
+                    accessibilityMap, startPoint, searchDistanceInMeters);
+
+            return determineAddedRestrictionsDifference(baseAccessibleIsochrone, accessibleRoadsWithRestrictions);
+        }
     }
 
     public SortedMap<Integer, RoadSection> determineAccessibilityByRoadSection(VehicleProperties vehicleProperties,
-            String municipalityId) {
+            String municipalityId, ResultType resultType) {
         AccessibilityMap accessibilityMap = accessibilityMapFactory.createMapMatcher(networkGraphHopper);
         Municipality municipality = municipalityService.getMunicipalityById(municipalityId);
 
         List<IsochroneMatch> accessibleRoadsWithRestrictions = accessibleRoadsService
                 .getVehicleAccessibleRoadsByMunicipality(accessibilityMap, vehicleProperties, municipality);
 
-        return determineDifference(accessibleRoadsWithRestrictions,() ->
-                accessibleRoadSectionsService.getRoadSectionIdToRoadSection(municipality.getMunicipalityIdInteger()));
+        if (resultType == ResultType.EFFECTIVE_ACCESSIBILITY) {
+            return determineUnion(accessibleRoadsWithRestrictions,
+                    () -> accessibleRoadSectionsService.getRoadSectionIdToRoadSection(
+                            municipality.getMunicipalityIdInteger()));
+        } else {
+            List<IsochroneMatch> baseAccessibleIsochrone = accessibleRoadsService.getBaseAccessibleRoadsByMunicipality(
+                    accessibilityMap, municipality);
+
+            return determineAddedRestrictionsDifference(baseAccessibleIsochrone, accessibleRoadsWithRestrictions);
+        }
     }
 
-    private SortedMap<Integer, RoadSection> determineDifference(List<IsochroneMatch> withRestrictions,
+    private SortedMap<Integer, RoadSection> determineAddedRestrictionsDifference(
+            List<IsochroneMatch> withoutRestrictions, List<IsochroneMatch> withRestrictions) {
+        SortedMap<Integer, RoadSection> roadSections = new TreeMap<>();
+        for (IsochroneMatch m : withoutRestrictions) {
+            // Graphhopper returns the geometry in the driving direction, we want this geometry in its original
+            // direction. Flip the geometry if isochrone returns the reversed as first result
+            roadSections.computeIfAbsent(m.getMatchedLinkId(), i -> new RoadSection(i,
+                    m.isReversed() ? m.getGeometry().reverse() : m.getGeometry()));
+            RoadSection r = roadSections.get(m.getMatchedLinkId());
+            // Accessible remains null in case road section is not present in both directions in baseline.
+            // This way, non-existing directions of one-way roads (null) can be distinguished from inaccessible
+            // directions due to restrictions (false).
+            if (m.isReversed()) {
+                r.setBackwardAccessible(ACCESSIBLE_ROAD_DIRECTION_IN_NWB_INITIALIZE_AS_INACCESSIBLE);
+            } else {
+                r.setForwardAccessible(ACCESSIBLE_ROAD_DIRECTION_IN_NWB_INITIALIZE_AS_INACCESSIBLE);
+            }
+        }
+        for (IsochroneMatch m : withRestrictions) {
+            RoadSection r = roadSections.get(m.getMatchedLinkId());
+            if (m.isReversed()) {
+                r.setBackwardAccessible(ACCESSIBLE_ROAD_DIRECTION_IN_ISOCHRONE_RESULT);
+            } else {
+                r.setForwardAccessible(ACCESSIBLE_ROAD_DIRECTION_IN_ISOCHRONE_RESULT);
+            }
+        }
+
+        return roadSections;
+    }
+
+    private SortedMap<Integer, RoadSection> determineUnion(List<IsochroneMatch> withRestrictions,
             Supplier<List<AccessibleRoadSection>> listSupplier) {
 
         // For every road driving direction that is accessible for a car in the NWB municipality area, we initialize our
--- accessibility/src/main/java/nu/ndw/nls/accessibilitymap/accessibility/services/AccessibleRoadsService.java
+++ accessibility/src/main/java/nu/ndw/nls/accessibilitymap/accessibility/services/AccessibleRoadsService.java
@@ -9,10 +9,36 @@ import nu.ndw.nls.accessibilitymap.accessibility.model.VehicleProperties;
 import nu.ndw.nls.accessibilitymap.accessibility.model.AccessibilityRequest;
 import nu.ndw.nls.routingmapmatcher.model.IsochroneMatch;
 import org.locationtech.jts.geom.Point;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AccessibleRoadsService {
+
+    @Cacheable(key = "#municipality.municipalityId", cacheNames = "baseAccessibleRoadsByMunicipality", sync = true)
+    @Timed(description = "Time spent determining base accessible road sections")
+    public List<IsochroneMatch> getBaseAccessibleRoads(AccessibilityMap accessibilityMap,  Point startPoint,
+            double searchDistanceInMeters ) {
+        AccessibilityRequest accessibilityRequest = AccessibilityRequest.builder()
+                .startPoint(startPoint)
+                .searchDistanceInMetres(searchDistanceInMeters)
+                .build();
+
+        return accessibilityMap.getAccessibleRoadSections(accessibilityRequest);
+    }
+
+    @Cacheable(key = "#municipality.municipalityId", cacheNames = "baseAccessibleRoadsByMunicipality", sync = true)
+    @Timed(description = "Time spent determining base accessible road sections")
+    public List<IsochroneMatch> getBaseAccessibleRoadsByMunicipality(AccessibilityMap accessibilityMap,
+            Municipality municipality) {
+        AccessibilityRequest accessibilityRequest = AccessibilityRequest.builder()
+                .startPoint(municipality.getStartPoint())
+                .municipalityId(municipality.getMunicipalityIdInteger())
+                .searchDistanceInMetres(municipality.getSearchDistanceInMetres())
+                .build();
+
+        return accessibilityMap.getAccessibleRoadSections(accessibilityRequest);
+    }
 
     @Timed(description = "Time spent determining vehicle accessible road sections within municipality")
     public List<IsochroneMatch> getVehicleAccessibleRoadsByMunicipality(AccessibilityMap accessibilityMap,
