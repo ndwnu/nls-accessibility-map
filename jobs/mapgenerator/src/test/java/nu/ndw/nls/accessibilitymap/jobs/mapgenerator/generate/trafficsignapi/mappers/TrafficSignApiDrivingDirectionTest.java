05650293c84f97e996efc809329e043aeccbd951
Guido Senff <167181728+ndwlocatieservices@users.noreply.github.com>
2024-08-22 13:20:08 +0000

    Merged PR 60001: #77699 Fixed driving direction mapping
    #77699 Fixed driving direction mapping
    
    Related work items: #77699

--- jobs/mapgenerator/src/test/java/nu/ndw/nls/accessibilitymap/jobs/mapgenerator/generate/trafficsignapi/mappers/TrafficSignApiDrivingDirectionTest.java
+++ jobs/mapgenerator/src/test/java/nu/ndw/nls/accessibilitymap/jobs/mapgenerator/generate/trafficsignapi/mappers/TrafficSignApiDrivingDirectionTest.java
@@ -2,6 +2,7 @@ package nu.ndw.nls.accessibilitymap.jobs.mapgenerator.generate.trafficsignapi.ma
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import nu.ndw.nls.accessibilitymap.jobs.mapgenerator.trafficsignapi.mappers.TrafficSignApiDrivingDirection;
@@ -19,12 +20,14 @@ class TrafficSignApiDrivingDirectionTest {
         assertEquals(TrafficSignApiDrivingDirection.FORWARD, TrafficSignApiDrivingDirection.from(DRIVING_DIRECTION_FORWARD));
         assertEquals(TrafficSignApiDrivingDirection.REVERSE, TrafficSignApiDrivingDirection.from(DRIVING_DIRECTION_REVERSE));
         assertEquals(TrafficSignApiDrivingDirection.BOTH, TrafficSignApiDrivingDirection.from(DRIVING_DIRECTION_BOTH_DIRECTIONS));
+        assertEquals(TrafficSignApiDrivingDirection.UNKNOWN, TrafficSignApiDrivingDirection.from(null));
     }
 
     @Test
     void isForward_ok() {
         assertTrue(TrafficSignApiDrivingDirection.FORWARD.isForward());
         assertTrue(TrafficSignApiDrivingDirection.BOTH.isForward());
+        assertTrue(TrafficSignApiDrivingDirection.UNKNOWN.isForward());
         assertFalse(TrafficSignApiDrivingDirection.REVERSE.isForward());
     }
 
@@ -32,7 +35,7 @@ class TrafficSignApiDrivingDirectionTest {
     void isReverse_ok() {
         assertTrue(TrafficSignApiDrivingDirection.REVERSE.isReverse());
         assertTrue(TrafficSignApiDrivingDirection.BOTH.isReverse());
-
+        assertTrue(TrafficSignApiDrivingDirection.UNKNOWN.isReverse());
         assertFalse(TrafficSignApiDrivingDirection.FORWARD.isReverse());
     }
 
@@ -41,6 +44,7 @@ class TrafficSignApiDrivingDirectionTest {
         assertEquals(DRIVING_DIRECTION_FORWARD, TrafficSignApiDrivingDirection.FORWARD.getValue());
         assertEquals(DRIVING_DIRECTION_REVERSE, TrafficSignApiDrivingDirection.REVERSE.getValue());
         assertEquals(DRIVING_DIRECTION_BOTH_DIRECTIONS, TrafficSignApiDrivingDirection.BOTH.getValue());
+        assertNull(TrafficSignApiDrivingDirection.UNKNOWN.getValue());
     }
 
 }
