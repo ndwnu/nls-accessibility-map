ee9832f8f4798dbc741fe652d72a1f298d70de2f
NDW Locatie Services <167181728+ndwlocatieservices@users.noreply.github.com>
2024-09-25 21:28:20 +0200

    #72404 - Cleanup

--- jobs/mapgenerator/src/main/java/nu/ndw/nls/accessibilitymap/jobs/mapgenerator/v2/model/MapGenerationProperties.java
+++ jobs/mapgenerator/src/main/java/nu/ndw/nls/accessibilitymap/jobs/mapgenerator/v2/model/MapGenerationProperties.java
@@ -1,10 +1,9 @@
 package nu.ndw.nls.accessibilitymap.jobs.mapgenerator.v2.model;
 
-import jakarta.validation.constraints.NotNull;
 import java.util.Set;
 import lombok.Builder;
 
 @Builder
-public record MapGenerationProperties(@NotNull Set<TrafficSignType> trafficSigns) {
+public record MapGenerationProperties(Set<TrafficSignType> trafficSigns) {
 
 }
--- jobs/mapgenerator/src/main/java/nu/ndw/nls/accessibilitymap/jobs/mapgenerator/v2/model/TrafficSign.java
+++ jobs/mapgenerator/src/main/java/nu/ndw/nls/accessibilitymap/jobs/mapgenerator/v2/model/TrafficSign.java
@@ -1,12 +1,11 @@
 package nu.ndw.nls.accessibilitymap.jobs.mapgenerator.v2.model;
 
-import jakarta.validation.constraints.NotNull;
 import lombok.Builder;
 
 @Builder
 public record TrafficSign(
-        @NotNull TrafficSignType trafficSignType,
-        @NotNull String windowTimes,
-        @NotNull Double nwbFraction) {
+        TrafficSignType trafficSignType,
+        String windowTimes,
+        Double nwbFraction) {
 
 }
