751d94781731375bed7c99e7204bf7254ba1b3af
Michel Durieux <167181728+ndwlocatieservices@users.noreply.github.com>
2024-09-25 14:53:26 +0000

    Merged PR 63620: 72404 v2 map generation
    - #72404 v2 map generation
    - #72404 v2 map generation
    - #72404 v2 map generation
    
    Related work items: #72404

--- jobs/mapgenerator/src/main/java/nu/ndw/nls/accessibilitymap/jobs/mapgenerator/v2/model/DirectionalSegment.java
+++ jobs/mapgenerator/src/main/java/nu/ndw/nls/accessibilitymap/jobs/mapgenerator/v2/model/DirectionalSegment.java
@@ -1,8 +1,9 @@
 package nu.ndw.nls.accessibilitymap.jobs.mapgenerator.v2.model;
 
+import lombok.Builder;
 import org.locationtech.jts.geom.LineString;
-
+@Builder
 public record DirectionalSegment(Direction direction, LineString lineString, TrafficSign trafficSign,
-                                 boolean accessible) {
+                                 Boolean accessible) {
 
 }
