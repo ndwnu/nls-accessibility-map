b8f75ca77cfbf87b676c5321d9f2d4fcc52442f9
m durieux <167181728+ndwlocatieservices@users.noreply.github.com>
2024-09-26 14:28:20 +0200

    #72404 v2 map generation

--- jobs/mapgenerator/src/main/java/nu/ndw/nls/accessibilitymap/jobs/mapgenerator/v2/mappers/TrafficSignMapper.java
+++ jobs/mapgenerator/src/main/java/nu/ndw/nls/accessibilitymap/jobs/mapgenerator/v2/mappers/TrafficSignMapper.java
@@ -1,18 +1,32 @@
 package nu.ndw.nls.accessibilitymap.jobs.mapgenerator.v2.mappers;
 
+import lombok.RequiredArgsConstructor;
 import nu.ndw.nls.accessibilitymap.jobs.mapgenerator.v2.model.TrafficSign;
 import nu.ndw.nls.accessibilitymap.jobs.mapgenerator.v2.model.TrafficSignType;
+import nu.ndw.nls.accessibilitymap.jobs.mapgenerator.v2.trafficsign.services.TextSignFilterService;
+import nu.ndw.nls.accessibilitymap.trafficsignclient.dtos.TextSignDto;
 import nu.ndw.nls.accessibilitymap.trafficsignclient.dtos.TrafficSignGeoJsonDto;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class TrafficSignMapper {
+   private final TextSignFilterService textSignFilterService;
 
     public TrafficSign mapFromTrafficSignGeoJsonDto(TrafficSignGeoJsonDto trafficSignGeoJsonDto) {
 
         return TrafficSign.builder()
                 .trafficSignType(TrafficSignType.valueOf(trafficSignGeoJsonDto.getProperties().getRvvCode()))
+                .windowTimes(findWindowTimes(trafficSignGeoJsonDto))
                 .fraction(trafficSignGeoJsonDto.getProperties().getFraction())
                 .build();
+    }
+
+    private String findWindowTimes(TrafficSignGeoJsonDto trafficSignGeoJsonDto) {
+        // @todo: replace getText() with getOpeningHours() when this text field is in use
+        return textSignFilterService.findFirstWindowTimeTextSign(trafficSignGeoJsonDto.getProperties().getTextSigns())
+                .map(TextSignDto::getText)
+                .orElseThrow(() -> new IllegalStateException("Failed to find window time text sign for traffic sign by "
+                        + "id: " + trafficSignGeoJsonDto.getId()));
     }
 }
