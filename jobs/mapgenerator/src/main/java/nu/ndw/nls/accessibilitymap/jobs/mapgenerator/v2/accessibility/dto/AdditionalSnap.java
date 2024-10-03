c11ac32fa7134fcfab3f90d24aba86665416f915
m durieux <167181728+ndwlocatieservices@users.noreply.github.com>
2024-10-03 14:59:07 +0200

    #72404 v2 map generation

--- jobs/mapgenerator/src/main/java/nu/ndw/nls/accessibilitymap/jobs/mapgenerator/v2/accessibility/dto/AdditionalSnap.java
+++ jobs/mapgenerator/src/main/java/nu/ndw/nls/accessibilitymap/jobs/mapgenerator/v2/accessibility/dto/AdditionalSnap.java
@@ -2,10 +2,14 @@ package nu.ndw.nls.accessibilitymap.jobs.mapgenerator.v2.accessibility.dto;
 
 import com.graphhopper.storage.index.Snap;
 import lombok.Builder;
+import lombok.Getter;
 import lombok.NonNull;
+import lombok.ToString;
 import nu.ndw.nls.accessibilitymap.jobs.mapgenerator.v2.model.trafficsign.TrafficSign;
 
 @Builder
+@Getter
+@ToString
 public class AdditionalSnap {
 
     @NonNull
