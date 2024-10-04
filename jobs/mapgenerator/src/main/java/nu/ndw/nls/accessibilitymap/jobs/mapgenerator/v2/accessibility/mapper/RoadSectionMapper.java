847967c7d602b993b14ba9d1ff58f744907f5f2e
Kevin Kamps <167181728+ndwlocatieservices@users.noreply.github.com>
2024-10-04 15:34:56 +0200

    #80293 - change traffic sign mapping to directionalSegments to encoded values

--- jobs/mapgenerator/src/main/java/nu/ndw/nls/accessibilitymap/jobs/mapgenerator/v2/accessibility/mapper/RoadSectionMapper.java
+++ jobs/mapgenerator/src/main/java/nu/ndw/nls/accessibilitymap/jobs/mapgenerator/v2/accessibility/mapper/RoadSectionMapper.java
@@ -1,5 +1,6 @@
 package nu.ndw.nls.accessibilitymap.jobs.mapgenerator.v2.accessibility.mapper;
 
+import com.graphhopper.routing.ev.IntEncodedValue;
 import java.util.Collection;
 import java.util.List;
 import java.util.Map;
@@ -11,7 +12,9 @@ import nu.ndw.nls.accessibilitymap.jobs.mapgenerator.v2.model.DirectionalSegment
 import nu.ndw.nls.accessibilitymap.jobs.mapgenerator.v2.model.RoadSection;
 import nu.ndw.nls.accessibilitymap.jobs.mapgenerator.v2.model.RoadSectionFragment;
 import nu.ndw.nls.accessibilitymap.jobs.mapgenerator.v2.model.trafficsign.TrafficSign;
+import nu.ndw.nls.accessibilitymap.shared.model.AccessibilityLink;
 import nu.ndw.nls.routingmapmatcher.model.IsochroneMatch;
+import nu.ndw.nls.routingmapmatcher.network.NetworkGraphHopper;
 import org.locationtech.jts.geom.LineString;
 import org.springframework.stereotype.Component;
 
@@ -19,17 +22,21 @@ import org.springframework.stereotype.Component;
 @Component
 public class RoadSectionMapper {
 
+    private NetworkGraphHopper networkGraphHopper;
+
     public Collection<RoadSection> mapToRoadSections(
             List<IsochroneMatch> isochroneMatches,
             Map<Integer, TrafficSign> trafficSignById) {
 
+        IntEncodedValue trafficSignEncodedValueAttribute = networkGraphHopper.getEncodingManager()
+                .getIntEncodedValue(AccessibilityLink.TRAFFIC_SIGN_ID);
         SortedMap<Integer, RoadSection> roadSectionsById = new TreeMap<>();
         SortedMap<Integer, RoadSectionFragment> roadSectionFragmentById = new TreeMap<>();
 
         isochroneMatches.forEach(isochroneMatch -> {
             int roadSectionId = isochroneMatch.getMatchedLinkId();
-            int roadSectionFragmentId = isochroneMatch.getEdgeId();
-            int directionalSegmentId = isochroneMatch.getEdgeKey();
+            int roadSectionFragmentId = isochroneMatch.getEdge().getEdge();
+            int directionalSegmentId = isochroneMatch.getEdge().getEdgeKey();
 
             RoadSection roadSection = roadSectionsById.computeIfAbsent(
                     roadSectionId,
@@ -56,7 +63,7 @@ public class RoadSectionMapper {
                                 Direction.BACKWARD,
                                 isochroneMatch.getGeometry(),
                                 roadSectionFragmentById.get(roadSectionFragmentId),
-                                trafficSignById.get(directionalSegmentId))); // TODO: replace with encodedValue
+                                trafficSignById.get(isochroneMatch.getEdge().get(trafficSignEncodedValueAttribute))));
             } else {
                 roadSectionFragment.getForwardSegments().add(
                         buildDirectionalSegment(
@@ -64,7 +71,7 @@ public class RoadSectionMapper {
                                 Direction.FORWARD,
                                 isochroneMatch.getGeometry(),
                                 roadSectionFragmentById.get(roadSectionFragmentId),
-                                trafficSignById.get(directionalSegmentId)));  // TODO: replace with encodedValue
+                                trafficSignById.get(isochroneMatch.getEdge().get(trafficSignEncodedValueAttribute))));
             }
         });
 
