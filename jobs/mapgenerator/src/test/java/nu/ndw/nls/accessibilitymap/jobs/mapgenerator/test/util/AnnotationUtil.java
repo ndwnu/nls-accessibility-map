c7cc36ac153d233a01841097c4ede8673f94b518
NDW Locatie Services <167181728+ndwlocatieservices@users.noreply.github.com>
2024-10-08 10:52:04 +0200

    #80293 - Code refactoring, introducting AccessibilityRequestMapper. Added .gitignore entry to ignore local profiles to override settings locally

--- jobs/mapgenerator/src/test/java/nu/ndw/nls/accessibilitymap/jobs/mapgenerator/test/util/AnnotationUtil.java
+++ jobs/mapgenerator/src/test/java/nu/ndw/nls/accessibilitymap/jobs/mapgenerator/test/util/AnnotationUtil.java
@@ -5,9 +5,28 @@ import static org.junit.jupiter.api.Assertions.fail;
 
 import java.lang.annotation.Annotation;
 import java.util.Arrays;
+import java.util.Objects;
 import java.util.function.Consumer;
 
 public class AnnotationUtil {
+
+	public static <ANNOTATION extends Annotation> void methodParameterContainsAnnotation(final Class<?> classToCheck, final Class<? extends ANNOTATION> annotationClass, final String methodToCheck, final String parameterToCheck, Consumer<ANNOTATION> annotationTestCallback) {
+
+		Arrays.stream(classToCheck.getDeclaredMethods())
+				.filter(declaredMethod -> declaredMethod.getName().equals(methodToCheck))
+				.map(method -> Arrays.stream(method.getParameters())
+						.filter(parameter -> parameter.getName().equals(parameterToCheck))
+						.map(parameter -> parameter.getDeclaredAnnotation(annotationClass))
+						.filter(Objects::nonNull)
+						.map(annotation -> annotationClass.cast(annotation))
+						.findFirst().orElse(null))
+				.filter(Objects::nonNull)
+				.findFirst()
+				.ifPresentOrElse(aroundAnnotation -> {
+							annotationTestCallback.accept(aroundAnnotation);
+						},
+						() -> fail(String.format("Missing annotation '%s' on method '%s' parameter '%s'", annotationClass, methodToCheck, parameterToCheck)));
+	}
 
 	public static <ANNOTATION extends Annotation> void methodsContainsAnnotation(final Class<?> classToCheck, Class<? extends ANNOTATION> annotationClass, final String methodName, Consumer<ANNOTATION> annotationTestCallback) {
 
