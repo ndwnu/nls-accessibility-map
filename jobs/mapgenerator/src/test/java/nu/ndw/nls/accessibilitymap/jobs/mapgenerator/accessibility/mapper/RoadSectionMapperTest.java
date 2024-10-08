870cd3b462a124900e674f15f3553fbef4cb1b67
NDW Locatie Services <167181728+ndwlocatieservices@users.noreply.github.com>
2024-10-08 22:11:22 +0200

    #80293 - Added tests

---
+++ jobs/mapgenerator/src/test/java/nu/ndw/nls/accessibilitymap/jobs/mapgenerator/accessibility/mapper/RoadSectionMapperTest.java
@@ -0,0 +1,115 @@
+package nu.ndw.nls.accessibilitymap.jobs.mapgenerator.accessibility.mapper;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
+
+import com.graphhopper.routing.ev.IntEncodedValue;
+import com.graphhopper.routing.util.EncodingManager;
+import com.graphhopper.util.EdgeIteratorState;
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+import nu.ndw.nls.accessibilitymap.jobs.mapgenerator.core.model.Direction;
+import nu.ndw.nls.accessibilitymap.jobs.mapgenerator.core.model.DirectionalSegment;
+import nu.ndw.nls.accessibilitymap.jobs.mapgenerator.core.model.RoadSection;
+import nu.ndw.nls.accessibilitymap.jobs.mapgenerator.core.model.RoadSectionFragment;
+import nu.ndw.nls.accessibilitymap.jobs.mapgenerator.core.model.trafficsign.TrafficSign;
+import nu.ndw.nls.accessibilitymap.shared.model.AccessibilityLink;
+import nu.ndw.nls.routingmapmatcher.model.IsochroneMatch;
+import nu.ndw.nls.routingmapmatcher.network.NetworkGraphHopper;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.locationtech.jts.geom.LineString;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+class RoadSectionMapperTest {
+
+    private RoadSectionMapper roadSectionMapper;
+
+    @Mock
+    private NetworkGraphHopper networkGraphHopper;
+
+    @Mock
+    private EncodingManager encodingManager;
+
+    @Mock
+    private IntEncodedValue intEncodedValue;
+
+    @Mock
+    private IsochroneMatch isochroneMatch;
+
+    @Mock
+    private EdgeIteratorState edgeIteratorState;
+
+    @Mock
+    private TrafficSign trafficSign;
+
+    @Mock
+    private LineString geometry;
+
+    @BeforeEach
+    void setUp() {
+
+        roadSectionMapper = new RoadSectionMapper(networkGraphHopper);
+    }
+
+    @ParameterizedTest
+    @CsvSource(textBlock = """
+            true,
+            false
+            """)
+    void mapToRoadSections(boolean isReversed) {
+
+        List<IsochroneMatch> isochroneMatches = List.of(isochroneMatch);
+        Map<Integer, TrafficSign> trafficSignById = Map.of(
+                10, trafficSign
+        );
+
+        when(networkGraphHopper.getEncodingManager()).thenReturn(encodingManager);
+        when(encodingManager.getIntEncodedValue(AccessibilityLink.TRAFFIC_SIGN_ID)).thenReturn(intEncodedValue);
+        when(isochroneMatch.getMatchedLinkId()).thenReturn(1);
+        when(isochroneMatch.getEdge()).thenReturn(edgeIteratorState);
+        when(edgeIteratorState.getEdge()).thenReturn(2);
+        when(edgeIteratorState.getEdgeKey()).thenReturn(3);
+        when(edgeIteratorState.get(intEncodedValue)).thenReturn(10);
+        when(isochroneMatch.isReversed()).thenReturn(isReversed);
+        when(isochroneMatch.getGeometry()).thenReturn(geometry);
+
+        Collection<RoadSection> roadSections = roadSectionMapper.mapToRoadSections(isochroneMatches, trafficSignById);
+
+        assertThat(roadSections).isNotEmpty();
+        assertThat(roadSections).hasSize(1);
+
+        RoadSection roadSection = roadSections.iterator().next();
+        assertThat(roadSection.getId()).isEqualTo(1);
+
+        assertThat(roadSection.getRoadSectionFragments()).hasSize(1);
+        RoadSectionFragment roadSectionFragment = roadSection.getRoadSectionFragments().getFirst();
+
+        assertThat(roadSectionFragment.getId()).isEqualTo(2);
+        assertThat(roadSectionFragment.getRoadSection()).isEqualTo(roadSection);
+
+        if (isReversed) {
+            validateSegments(roadSectionFragment.getBackwardSegments(), roadSectionFragment, Direction.BACKWARD);
+            assertThat(roadSectionFragment.getForwardSegments()).isEmpty();
+        } else {
+            validateSegments(roadSectionFragment.getForwardSegments(), roadSectionFragment, Direction.FORWARD);
+            assertThat(roadSectionFragment.getBackwardSegments()).isEmpty();
+        }
+    }
+
+    private void validateSegments(List<DirectionalSegment> segments, RoadSectionFragment roadSectionFragment, Direction direction) {
+
+        assertThat(segments).hasSize(1);
+        assertThat(segments.getFirst().getId()).isEqualTo(3);
+        assertThat(segments.getFirst().getDirection()).isEqualTo(direction);
+        assertThat(segments.getFirst().getTrafficSign()).isEqualTo(trafficSign);
+        assertThat(segments.getFirst().getRoadSectionFragment()).isEqualTo(roadSectionFragment);
+        assertThat(segments.getFirst().isAccessible()).isTrue();
+        assertThat(segments.getFirst().getLineString()).isEqualTo(geometry);
+    }
+}
