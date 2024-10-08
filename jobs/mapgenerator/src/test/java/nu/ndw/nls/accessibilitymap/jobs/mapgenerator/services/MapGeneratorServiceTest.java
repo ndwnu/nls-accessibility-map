2bde441104489796c8c87c89c8f94c8558095e10
NDW Locatie Services <167181728+ndwlocatieservices@users.noreply.github.com>
2024-10-08 10:42:41 +0200

    #80293 - Fixed compilation error.

--- jobs/mapgenerator/src/test/java/nu/ndw/nls/accessibilitymap/jobs/mapgenerator/services/MapGeneratorServiceTest.java
+++ jobs/mapgenerator/src/test/java/nu/ndw/nls/accessibilitymap/jobs/mapgenerator/services/MapGeneratorServiceTest.java
@@ -2,6 +2,7 @@ package nu.ndw.nls.accessibilitymap.jobs.mapgenerator.services;
 
 import java.util.List;
 import nu.ndw.nls.accessibilitymap.jobs.mapgenerator.accessibility.AccessibilityService;
+import nu.ndw.nls.accessibilitymap.jobs.mapgenerator.core.time.ClockService;
 import nu.ndw.nls.accessibilitymap.jobs.mapgenerator.event.AccessibilityGeoJsonGeneratedEventMapper;
 import nu.ndw.nls.accessibilitymap.jobs.mapgenerator.geojson.writers.OutputWriter;
 import nu.ndw.nls.springboot.messaging.services.MessageService;
@@ -14,22 +15,26 @@ class MapGeneratorServiceTest {
 
     private MapGeneratorService mapGeneratorService;
 
-	private AccessibilityGeoJsonGeneratedEventMapper accessibilityGeoJsonGeneratedEventMapper;
+    private AccessibilityGeoJsonGeneratedEventMapper accessibilityGeoJsonGeneratedEventMapper;
 
-	private List<OutputWriter> outputWriters;
+    private List<OutputWriter> outputWriters;
 
-	private AccessibilityService accessibilityService;
+    private AccessibilityService accessibilityService;
 
-	private MessageService messageService;
+    private MessageService messageService;
+
+    private ClockService clockService;
+
     @BeforeEach
     void setUp() {
 
-		mapGeneratorService = new MapGeneratorService(
-				accessibilityGeoJsonGeneratedEventMapper,
-				outputWriters,
-				accessibilityService,
-				messageService
-		);
+        mapGeneratorService = new MapGeneratorService(
+                accessibilityGeoJsonGeneratedEventMapper,
+                outputWriters,
+                accessibilityService,
+                messageService,
+				clockService
+        );
     }
 
 
