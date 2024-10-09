4eb7b3197c031e06cf4e37f1cb3c3f5c84f926ef
Kevin Kamps <167181728+ndwlocatieservices@users.noreply.github.com>
2024-10-09 16:31:37 +0200

    #80293 - Code cleanup / added more tests.

---
+++ jobs/mapgenerator/src/test/java/nu/ndw/nls/accessibilitymap/jobs/mapgenerator/command/BaseLoaderCommandTest.java
@@ -0,0 +1,34 @@
+package nu.ndw.nls.accessibilitymap.jobs.mapgenerator.command;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import nu.ndw.nls.accessibilitymap.jobs.mapgenerator.test.util.AnnotationUtil;
+import org.junit.jupiter.api.Test;
+import org.springframework.stereotype.Component;
+import picocli.CommandLine.Command;
+
+class BaseLoaderCommandTest {
+
+    @Test
+    void annotation_class_component() {
+
+        AnnotationUtil.classContainsAnnotation(
+                BaseLoaderCommand.class,
+                Component.class,
+                annotation -> assertThat(annotation).isNotNull()
+        );
+    }
+
+    @Test
+    void annotation_class_Command() {
+
+        AnnotationUtil.classContainsAnnotation(
+                BaseLoaderCommand.class,
+                Command.class,
+                annotation -> {
+                    assertThat(annotation.name()).isEqualTo("jobs");
+                    assertThat(annotation.subcommands()).containsExactlyInAnyOrder(GenerateGeoJsonCommand.class);
+                }
+        );
+    }
+}
