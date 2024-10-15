2031369b5f69d7fdef7facc562bd61cd139dc254
m durieux <167181728+ndwlocatieservices@users.noreply.github.com>
2024-10-15 15:19:46 +0200

    #80293 added unit tests for QueryGraphConfigurer and QueryGraphFactory

---
+++ jobs/mapgenerator/src/test/java/nu/ndw/nls/accessibilitymap/jobs/mapgenerator/graphhopper/QueryGraphFactoryTest.java
@@ -0,0 +1,68 @@
+package nu.ndw.nls.accessibilitymap.jobs.mapgenerator.graphhopper;
+
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import com.graphhopper.routing.querygraph.QueryGraph;
+import com.graphhopper.storage.BaseGraph;
+import com.graphhopper.storage.index.Snap;
+import java.util.List;
+import nu.ndw.nls.accessibilitymap.jobs.mapgenerator.accessibility.dto.TrafficSignSnap;
+import nu.ndw.nls.routingmapmatcher.network.NetworkGraphHopper;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockedStatic;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+class QueryGraphFactoryTest {
+
+    @Mock
+    private QueryGraphConfigurer queryGraphConfigurer;
+    @Mock
+    private NetworkGraphHopper networkGraphHopper;
+
+    @Mock
+    private BaseGraph baseGraph;
+    @Mock
+    private Snap startSegment;
+    @Mock
+    private Snap snapForTrafficSign;
+    @Mock
+    private TrafficSignSnap trafficSignSnap;
+    @Mock
+    private QueryGraph queryGraph;
+    ;
+
+    private static MockedStatic<QueryGraph> queryGraphStaticMock;
+
+    @InjectMocks
+    private QueryGraphFactory queryGraphFactory;
+
+    @BeforeAll
+    static void setUp() {
+        queryGraphStaticMock = Mockito.mockStatic(QueryGraph.class);
+    }
+
+    @AfterAll
+    static void tearDown() {
+        queryGraphStaticMock.close();
+    }
+
+    @Test
+    void createQueryGraph_ok() {
+        when(networkGraphHopper.getBaseGraph()).thenReturn(baseGraph);
+        when(trafficSignSnap.getSnap()).thenReturn(snapForTrafficSign);
+        queryGraphStaticMock.when(() -> QueryGraph.create(baseGraph, List.of(snapForTrafficSign,startSegment))).thenReturn(queryGraph);
+        QueryGraph result = queryGraphFactory.createQueryGraph(List.of(trafficSignSnap), startSegment);
+        assertThat(result).isEqualTo(queryGraph);
+        verify(queryGraphConfigurer).configure(queryGraph,List.of(trafficSignSnap));
+    }
+}
