c42884656ef1658f17817d2b806f198927869d92
Kevin Kamps <167181728+ndwlocatieservices@users.noreply.github.com>
2024-10-22 13:22:59 +0200

    #77004 - Fixed sonar issues. updated MapGeneratorJobsIT to use assertj

--- jobs/mapgenerator/src/main/java/nu/ndw/nls/accessibilitymap/jobs/mapgenerator/geojson/utils/polygon/DirectionalSegmentAccessibleComparator.java
+++ jobs/mapgenerator/src/main/java/nu/ndw/nls/accessibilitymap/jobs/mapgenerator/geojson/utils/polygon/DirectionalSegmentAccessibleComparator.java
@@ -1,11 +1,12 @@
 package nu.ndw.nls.accessibilitymap.jobs.mapgenerator.geojson.utils.polygon;
 
+import java.io.Serializable;
 import java.util.Comparator;
 import nu.ndw.nls.accessibilitymap.jobs.mapgenerator.core.dto.DirectionalSegment;
 import org.springframework.stereotype.Component;
 
 @Component
-public class DirectionalSegmentAccessibleComparator implements Comparator<DirectionalSegment> {
+public class DirectionalSegmentAccessibleComparator implements Comparator<DirectionalSegment>, Serializable {
 
     @Override
     public int compare(DirectionalSegment directionalSegment1, DirectionalSegment directionalSegment2) {
--- jobs/mapgenerator/src/test-integration/java/nu/ndw/nls/accessibilitymap/jobs/mapgenerator/MapGeneratorJobsIT.java
+++ jobs/mapgenerator/src/test-integration/java/nu/ndw/nls/accessibilitymap/jobs/mapgenerator/MapGeneratorJobsIT.java
@@ -2,8 +2,7 @@ package nu.ndw.nls.accessibilitymap.jobs.mapgenerator;
 
 import static net.javacrumbs.jsonunit.assertj.JsonAssertions.assertThatJson;
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.assertj.core.api.Fail.fail;
 
 import java.io.File;
 import java.io.IOException;
@@ -12,7 +11,10 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
-import nu.ndw.nls.accessibilitymap.jobs.mapgenerator.MapGeneratorJobsIT.TestConfig;
+import java.util.Locale;
+import java.util.Map;
+import nu.ndw.nls.accessibilitymap.jobs.mapgenerator.MapGeneratorJobTest.TestConfig;
+import nu.ndw.nls.accessibilitymap.jobs.mapgenerator.core.dto.trafficsign.TrafficSignType;
 import nu.ndw.nls.events.NlsEvent;
 import nu.ndw.nls.events.NlsEventSubjectType;
 import nu.ndw.nls.events.NlsEventType;
@@ -21,7 +23,8 @@ import nu.ndw.nls.springboot.messaging.dtos.MessageConsumeResult;
 import nu.ndw.nls.springboot.messaging.services.MessageReceiveService.ReceiveKey;
 import nu.ndw.nls.springboot.messaging.services.MessageService;
 import org.apache.commons.io.FileUtils;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.EnumSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Configuration;
@@ -33,69 +36,54 @@ import org.springframework.util.ResourceUtils;
 @SpringBootTest
 @ContextConfiguration(classes = TestConfig.class)
 @ActiveProfiles(profiles = {"integration-test"})
-class MapGeneratorJobsIT {
+class MapGeneratorJobTest {
 
     private static final String DESTINATION_PATH = "../../map-generation-destination";
 
-    @Configuration
-    @Import(MessagingConfig.class)
-    public static class TestConfig {
-
-    }
+    private static final Map<TrafficSignType, NlsEventSubjectType> TRAFFIC_SIGN_TYPE_NLS_EVENT_SUBJECT_TYPE_MAP = Map.of(
+            TrafficSignType.C6, NlsEventSubjectType.ACCESSIBILITY_WINDOWS_TIMES_RVV_CODE_C6,
+            TrafficSignType.C7, NlsEventSubjectType.ACCESSIBILITY_WINDOWS_TIMES_RVV_CODE_C7,
+            TrafficSignType.C7B, NlsEventSubjectType.ACCESSIBILITY_WINDOWS_TIMES_RVV_CODE_C7B,
+            TrafficSignType.C12, NlsEventSubjectType.ACCESSIBILITY_WINDOWS_TIMES_RVV_CODE_C12,
+            TrafficSignType.C22C, NlsEventSubjectType.ACCESSIBILITY_WINDOWS_TIMES_RVV_CODE_C22C
+    );
 
     @Autowired
     private MessageService messageService;
 
-    @Test
-    void messageReceived_ok_c6Published() {
+    @ParameterizedTest
+    @EnumSource(TrafficSignType.class)
+    void messageReceived_ok(TrafficSignType trafficSignType) {
 
-        verifyMessageAvailable(NlsEventSubjectType.ACCESSIBILITY_WINDOWS_TIMES_RVV_CODE_C6);
+        if (!TRAFFIC_SIGN_TYPE_NLS_EVENT_SUBJECT_TYPE_MAP.containsKey(trafficSignType)) {
+            fail("Missing a topic for traffic sign type: %s".formatted(trafficSignType.name()));
+        }
+
+        verifyMessageAvailable(TRAFFIC_SIGN_TYPE_NLS_EVENT_SUBJECT_TYPE_MAP.get(trafficSignType));
     }
 
-    @Test
-    void messageReceived_ok_c7Published() {
+    @ParameterizedTest
+    @EnumSource(TrafficSignType.class)
+    void geojsonPublished_ok(TrafficSignType trafficSignType) throws IOException {
 
-        verifyMessageAvailable(NlsEventSubjectType.ACCESSIBILITY_WINDOWS_TIMES_RVV_CODE_C7);
+        verifyGeoJson(trafficSignType.name().toLowerCase(Locale.US).concat("WindowTimeSegments.geojson"));
+        verifyGeoJson(trafficSignType.name().toLowerCase(Locale.US).concat("WindowTimeSegments-polygon.geojson"));
     }
 
-    @Test
-    void messageReceived_ok_c7bPublished() {
-
-        verifyMessageAvailable(NlsEventSubjectType.ACCESSIBILITY_WINDOWS_TIMES_RVV_CODE_C7B);
-    }
-
-    @Test
-    void messageReceived_ok_c12Published() {
-
-        verifyMessageAvailable(NlsEventSubjectType.ACCESSIBILITY_WINDOWS_TIMES_RVV_CODE_C12);
-    }
-
-    @Test
-    void messageReceived_ok_c22cPublished() {
-
-        verifyMessageAvailable(NlsEventSubjectType.ACCESSIBILITY_WINDOWS_TIMES_RVV_CODE_C22C);
-    }
-
-    @Test
-    void geojson_ok_c6Published() throws IOException {
-
-        verifyGeoJson("c6WindowTimeSegments.geojson");
-        verifyGeoJson("c6WindowTimeSegments-polygon.geojson");
-    }
     private void verifyGeoJson(String geojsonFileName) throws IOException {
         Path geojsonFilePath = formatGeneratedWindowTimesPath(geojsonFileName);
         assertThat(Files.exists(geojsonFilePath))
                 .isTrue()
                 .withFailMessage("GeoJson file does not exist");
 
-        assertTrue(Files.size(geojsonFilePath) > 0, "GeoJson file must not be 0 bytes");
+        assertThat(Files.size(geojsonFilePath))
+                .isGreaterThan(0)
+                .withFailMessage("GeoJson file must be larger than 0 bytes");
 
         String actualJson = Files.readString(geojsonFilePath);
-
         String expectedJson = readTestDataFromFile("expected-it-results/MapGeneratorJobsIT", geojsonFileName);
 
-        assertThatJson(actualJson)
-                .isEqualTo(expectedJson);
+        assertThatJson(actualJson).isEqualTo(expectedJson);
     }
 
     private void verifyMessageAvailable(NlsEventSubjectType nlsEventSubjectType) {
@@ -107,10 +95,11 @@ class MapGeneratorJobsIT {
 
         NlsEvent result = messageConsumeResult.getResult();
 
-        assertEquals(NlsEventType.MAP_GEOJSON_PUBLISHED_EVENT, result.getType());
-        assertEquals(nlsEventSubjectType, result.getSubject().getType());
-        assertEquals(LocalDate.now().format(DateTimeFormatter.BASIC_ISO_DATE), result.getSubject().getVersion());
-        assertEquals("20240701", result.getSubject().getNwbVersion());
+        assertThat(result.getType()).isEqualTo(NlsEventType.MAP_GEOJSON_PUBLISHED_EVENT);
+        assertThat(result.getSubject().getType()).isEqualTo(nlsEventSubjectType);
+        assertThat(result.getSubject().getVersion())
+                .isEqualTo(LocalDate.now().format(DateTimeFormatter.BASIC_ISO_DATE));
+        assertThat(result.getSubject().getNwbVersion()).isEqualTo("20240701");
     }
 
     private Path formatGeneratedWindowTimesPath(String geojsonFileName) {
@@ -127,5 +116,10 @@ class MapGeneratorJobsIT {
                 StandardCharsets.UTF_8.toString());
     }
 
+    @Configuration
+    @Import(MessagingConfig.class)
+    public static class TestConfig {
+
+    }
 }
 
