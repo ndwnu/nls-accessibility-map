3765df78b0ecaaada522a12fe7d2e7d52c8c5a5d
Kevin Kamps <167181728+ndwlocatieservices@users.noreply.github.com>
2025-01-22 17:23:53 +0100

    [#86823] - revert script changes

--- scripts/update-maven-version.sh
+++ scripts/update-maven-version.sh
@@ -13,6 +13,3 @@ select selected in "${options[@]}"; do
   *) echo "invalid option $REPLY" ;;
   esac
 done
-
-
-mvn build-helper:parse-version versions:set -DnewVersion=4.1.1-SNAPSHOT -DgenerateBackupPoms=false
