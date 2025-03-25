e49258f8a2e3531ed01166863e5c3ec3d04171cd
NDW Locatie Services <167181728+ndwlocatieservices@users.noreply.github.com>
2025-03-25 08:40:02 +0100

    #91446 Refactor edge restriction handling and improve logging
    Renamed method `hasEdgeRestriction` to `hasEdgeRestrictions` for clarity and adjusted related method logic for accuracy. Introduced a stopwatch to log QueryGraph configuration timing, enhancing monitoring. Updated test cases to match the renaming and ensure consistent behavior.

--- accessibility/src/test/java/nu/ndw/nls/accessibilitymap/accessibility/graphhopper/querygraph/QueryGraphConfigurerTest.java
+++ accessibility/src/test/java/nu/ndw/nls/accessibilitymap/accessibility/graphhopper/querygraph/QueryGraphConfigurerTest.java
@@ -132,7 +132,7 @@ class QueryGraphConfigurerTest {
 
         TrafficSignEdgeRestrictions restrictions = queryGraphConfigurer.createEdgeRestrictions(queryGraph, List.of(trafficSignSnap));
 
-        assertThat(restrictions.hasEdgeRestriction(edgeIterator.getEdgeKey())).isTrue();
+        assertThat(restrictions.hasEdgeRestrictions(edgeIterator.getEdgeKey())).isTrue();
         TrafficSignEdgeRestriction restriction = restrictions.getEdgeRestriction(edgeIterator.getEdgeKey());
         assertThat(restriction.getTrafficSignId()).isEqualTo(TRAFFIC_SIGN_ID_VALUE);
     }
@@ -150,7 +150,7 @@ class QueryGraphConfigurerTest {
 
         TrafficSignEdgeRestrictions restrictions = queryGraphConfigurer.createEdgeRestrictions(queryGraph, List.of(trafficSignSnap));
 
-        assertThat(restrictions.hasEdgeRestriction(edgeIterator.getEdgeKey())).isFalse();
+        assertThat(restrictions.hasEdgeRestrictions(edgeIterator.getEdgeKey())).isFalse();
     }
 
     @Test
@@ -177,7 +177,7 @@ class QueryGraphConfigurerTest {
 
         TrafficSignEdgeRestrictions restrictions = queryGraphConfigurer.createEdgeRestrictions(queryGraph, List.of(trafficSignSnap));
 
-        assertThat(restrictions.hasEdgeRestriction(edgeIterator.getEdgeKey())).isFalse();
+        assertThat(restrictions.hasEdgeRestrictions(edgeIterator.getEdgeKey())).isFalse();
     }
 
 
