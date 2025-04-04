27548041b75e91fa9ddaa023180b1f1172a22054
m durieux <167181728+ndwlocatieservices@users.noreply.github.com>
2025-04-04 13:07:40 +0200

    #90886  Add bounding box mapping and unit tests for mappers

--- backend/src/main/java/nu/ndw/nls/accessibilitymap/backend/mappers/AccessibilityRequestV2Mapper.java
+++ backend/src/main/java/nu/ndw/nls/accessibilitymap/backend/mappers/AccessibilityRequestV2Mapper.java
@@ -1,10 +1,10 @@
 package nu.ndw.nls.accessibilitymap.backend.mappers;
 
+import com.graphhopper.util.shapes.BBox;
 import lombok.RequiredArgsConstructor;
 import nu.ndw.nls.accessibilitymap.accessibility.core.dto.request.AccessibilityRequest;
 import nu.ndw.nls.accessibilitymap.backend.controllers.dto.VehicleArguments;
 import nu.ndw.nls.accessibilitymap.backend.municipality.model.Municipality;
-import nu.ndw.nls.accessibilitymap.backend.validators.PointValidator;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -18,14 +18,24 @@ public class AccessibilityRequestV2Mapper {
         return AccessibilityRequest
                 .builder()
                 .municipalityId(municipality.getMunicipalityIdInteger())
+                .boundingBox(BBox.fromPoints(
+                        municipality.getBounds().latitudeFrom(),
+                        municipality.getBounds().latitudeFrom(),
+                        municipality.getBounds().latitudeTo(),
+                        municipality.getBounds().longitudeTo()
+                ))
                 .searchRadiusInMeters(municipality.getSearchDistanceInMetres())
                 .startLocationLatitude(municipality.getStartPoint().getY())
                 .startLocationLongitude(municipality.getStartPoint().getX())
-                .vehicleAxleLoadInKg(vehicleArguments.vehicleAxleLoad() != null ? Double.valueOf(vehicleArguments.vehicleAxleLoad()) : null)
-                .vehicleHeightInCm(vehicleArguments.vehicleHeight() != null ? Double.valueOf(vehicleArguments.vehicleHeight()) : null)
-                .vehicleLengthInCm(vehicleArguments.vehicleLength() != null ? Double.valueOf(vehicleArguments.vehicleLength()) : null)
+                .vehicleAxleLoadInKg(
+                        vehicleArguments.vehicleAxleLoad() != null ? Double.valueOf(vehicleArguments.vehicleAxleLoad()) : null)
+                .vehicleHeightInCm(
+                        vehicleArguments.vehicleHeight() != null ? Double.valueOf(vehicleArguments.vehicleHeight()) : null)
+                .vehicleLengthInCm(
+                        vehicleArguments.vehicleLength() != null ? Double.valueOf(vehicleArguments.vehicleLength()) : null)
                 .vehicleWidthInCm(vehicleArguments.vehicleWidth() != null ? Double.valueOf(vehicleArguments.vehicleWidth()) : null)
-                .vehicleWeightInKg(vehicleArguments.vehicleWeight() != null ? Double.valueOf(vehicleArguments.vehicleWeight()) : null)
+                .vehicleWeightInKg(
+                        vehicleArguments.vehicleWeight() != null ? Double.valueOf(vehicleArguments.vehicleWeight()) : null)
                 .transportTypes(transportTypeV2Mapper.mapToTransportType(vehicleArguments))
                 .build();
     }
