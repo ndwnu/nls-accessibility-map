1bd71c14858dd30821077128f29874613dd795dc
m durieux <167181728+ndwlocatieservices@users.noreply.github.com>
2025-04-11 09:37:41 +0200

    #90886 Refactor package structure for traffic sign jobs
    Renamed `trafficsignanalyser` to `trafficsign` across all files for a streamlined and consistent package structure.

---
+++ jobs/traffic-sign/src/main/java/nu/ndw/nls/accessibilitymap/jobs/trafficsign/Application.java
@@ -0,0 +1,19 @@
+package nu.ndw.nls.accessibilitymap.jobs.trafficsign;
+
+import nu.ndw.nls.geojson.geometry.JtsGeoJsonMappersConfiguration;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Import;
+
+@SpringBootApplication
+@EnableConfigurationProperties
+@ConfigurationPropertiesScan
+@Import({JtsGeoJsonMappersConfiguration.class})
+public class Application {
+
+    public static void main(String[] args) {
+        System.exit(SpringApplication.exit(SpringApplication.run(Application.class, args)));
+    }
+}
