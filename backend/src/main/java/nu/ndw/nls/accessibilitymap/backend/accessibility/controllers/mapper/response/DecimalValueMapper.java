18388035467971b86ad29187cea431a4db9073e6
NDW Locatie Services <167181728+ndwlocatieservices@users.noreply.github.com>
2025-08-18 16:02:55 +0200

    #98958 Added unit tests for `DecimalValueMapper`, extended tests for `TransportTypeMapper` and `FuelTypeMapper`, and adjusted constants in `DecimalValueMapper`.
    Related work items: #98958

--- backend/src/main/java/nu/ndw/nls/accessibilitymap/backend/accessibility/controllers/mapper/response/DecimalValueMapper.java
+++ backend/src/main/java/nu/ndw/nls/accessibilitymap/backend/accessibility/controllers/mapper/response/DecimalValueMapper.java
@@ -5,9 +5,11 @@ import java.math.RoundingMode;
 
 public class DecimalValueMapper {
 
+    private static final int NEW_SCALE = 2;
+
     public static BigDecimal mapToValue(Double value, int decimalPlaces) {
         return BigDecimal.valueOf(value)
                 .movePointLeft(decimalPlaces)
-                .setScale(2, RoundingMode.HALF_UP);
+                .setScale(NEW_SCALE, RoundingMode.HALF_UP);
     }
 }
