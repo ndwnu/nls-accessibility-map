ad2557c75df09fd9ccdb6138527986f7422d7171
NLS Build Agent <167181728+ndwlocatieservices@users.noreply.github.com>
2025-08-18 16:50:48 +0200

    [#98958] Fixed AccessibilityRequestTest test and moved PointMapper and PointMatchService

---
+++ accessibility/src/main/java/nu/ndw/nls/accessibilitymap/accessibility/service/AccessibilityException.java
@@ -0,0 +1,9 @@
+package nu.ndw.nls.accessibilitymap.accessibility.service;
+
+public class AccessibilityException extends RuntimeException {
+
+    public AccessibilityException(String message) {
+
+        super(message);
+    }
+}
