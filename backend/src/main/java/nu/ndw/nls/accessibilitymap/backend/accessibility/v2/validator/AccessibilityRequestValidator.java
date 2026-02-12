8b08c7d2c0d1b41972e585b71d06e66583d04b24
NDW Locatie Services <167181728+ndwlocatieservices@users.noreply.github.com>
2026-02-12 18:40:17 +0100

    [#108429] Added verifications on accessibility request when a bounding box area is defined also from must be defined. Fixed a bug in the error controller.

--- backend/src/main/java/nu/ndw/nls/accessibilitymap/backend/accessibility/v2/validator/AccessibilityRequestValidator.java
+++ backend/src/main/java/nu/ndw/nls/accessibilitymap/backend/accessibility/v2/validator/AccessibilityRequestValidator.java
@@ -5,6 +5,7 @@ import java.util.Objects;
 import java.util.UUID;
 import java.util.function.Predicate;
 import nu.ndw.nls.accessibilitymap.backend.openapi.model.v2.AccessibilityRequestJson;
+import nu.ndw.nls.accessibilitymap.backend.openapi.model.v2.BoundingBoxAreaRequestJson;
 import nu.ndw.nls.accessibilitymap.backend.openapi.model.v2.EmissionClassJson;
 import nu.ndw.nls.accessibilitymap.backend.openapi.model.v2.FuelTypeJson;
 import nu.ndw.nls.springboot.web.error.exceptions.ApiException;
@@ -18,6 +19,7 @@ public class AccessibilityRequestValidator {
     public boolean verify(AccessibilityRequestJson accessibilityRequest) {
 
         return meetsEmissionClassAndFuelTypeRequirements()
+                .and(whenBoundingBoxAreaRequestFromMustBeDefined())
                 .test(accessibilityRequest);
     }
 
@@ -34,10 +36,26 @@ public class AccessibilityRequestValidator {
             if ((Objects.isNull(emissionClass) && !CollectionUtils.isEmpty(fuelTypes))
                 || (CollectionUtils.isEmpty(fuelTypes) && Objects.nonNull(emissionClass))) {
                 throw new ApiException(
-                        UUID.randomUUID(),
+                        UUID.fromString("8091b9dc-c15f-4b74-8b45-0bf7605042be"),
                         HttpStatus.BAD_REQUEST,
                         "Invalid Request",
                         "If one of the environmental zone parameters is set, the other must be set as well.");
+            }
+            return true;
+        };
+    }
+
+    @SuppressWarnings("java:S1067")
+    private Predicate<AccessibilityRequestJson> whenBoundingBoxAreaRequestFromMustBeDefined() {
+
+        return accessibilityRequest -> {
+            if (accessibilityRequest.getArea() instanceof BoundingBoxAreaRequestJson
+                && Objects.isNull(accessibilityRequest.getFrom())) {
+                throw new ApiException(
+                        UUID.fromString("43b85771-22ad-490b-95c8-e659fb3fc915"),
+                        HttpStatus.BAD_REQUEST,
+                        "Invalid Request",
+                        "When using a bounding box area request, the from parameter must also be set.");
             }
             return true;
         };
