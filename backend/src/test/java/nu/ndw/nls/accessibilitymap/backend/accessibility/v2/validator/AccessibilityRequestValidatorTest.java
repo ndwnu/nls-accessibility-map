6b997ff34305281b3080dca1cfb68886e03f6f8a
Kevin Kamps <167181728+ndwlocatieservices@users.noreply.github.com>
2026-02-12 18:40:17 +0100

    [#108429] Added verifications on accessibility request when a bounding box area is defined also from must be defined. Fixed a bug in the error controller.

--- backend/src/test/java/nu/ndw/nls/accessibilitymap/backend/accessibility/v2/validator/AccessibilityRequestValidatorTest.java
+++ backend/src/test/java/nu/ndw/nls/accessibilitymap/backend/accessibility/v2/validator/AccessibilityRequestValidatorTest.java
@@ -3,9 +3,12 @@ package nu.ndw.nls.accessibilitymap.backend.accessibility.v2.validator;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.List;
+import java.util.UUID;
 import nu.ndw.nls.accessibilitymap.backend.openapi.model.v2.AccessibilityRequestJson;
+import nu.ndw.nls.accessibilitymap.backend.openapi.model.v2.BoundingBoxAreaRequestJson;
 import nu.ndw.nls.accessibilitymap.backend.openapi.model.v2.EmissionClassJson;
 import nu.ndw.nls.accessibilitymap.backend.openapi.model.v2.FuelTypeJson;
+import nu.ndw.nls.accessibilitymap.backend.openapi.model.v2.LocationJson;
 import nu.ndw.nls.accessibilitymap.backend.openapi.model.v2.VehicleCharacteristicsJson;
 import nu.ndw.nls.springboot.web.error.exceptions.ApiException;
 import org.junit.jupiter.api.BeforeEach;
@@ -52,7 +55,7 @@ class AccessibilityRequestValidatorTest {
             accessibilityRequestValidator.verify(accessibilityRequestJson);
         } catch (ApiException exception) {
 
-            assertThat(exception.getErrorId()).isNotNull();
+            assertThat(exception.getErrorId()).isEqualTo(UUID.fromString("06d84c7c-7be2-4f79-a8fd-00264f06267d"));
             assertThat(exception.getStatusCode()).isEqualTo(HttpStatus.BAD_REQUEST);
             assertThat(exception.getTitle()).isEqualTo("Invalid Request");
             assertThat(exception.getDescription())
@@ -74,7 +77,7 @@ class AccessibilityRequestValidatorTest {
             accessibilityRequestValidator.verify(accessibilityRequestJson);
         } catch (ApiException exception) {
 
-            assertThat(exception.getErrorId()).isNotNull();
+            assertThat(exception.getErrorId()).isEqualTo(UUID.fromString("06d84c7c-7be2-4f79-a8fd-00264f06267d"));
             assertThat(exception.getStatusCode()).isEqualTo(HttpStatus.BAD_REQUEST);
             assertThat(exception.getTitle()).isEqualTo("Invalid Request");
             assertThat(exception.getDescription())
@@ -96,11 +99,49 @@ class AccessibilityRequestValidatorTest {
             accessibilityRequestValidator.verify(accessibilityRequestJson);
         } catch (ApiException exception) {
 
-            assertThat(exception.getErrorId()).isNotNull();
+            assertThat(exception.getErrorId()).isEqualTo(UUID.fromString("06d84c7c-7be2-4f79-a8fd-00264f06267d"));
             assertThat(exception.getStatusCode()).isEqualTo(HttpStatus.BAD_REQUEST);
             assertThat(exception.getTitle()).isEqualTo("Invalid Request");
             assertThat(exception.getDescription())
                     .isEqualTo("If one of the environmental zone parameters is set, the other must be set as well.");
         }
     }
+
+
+    @Test
+    void verify_boundingBoxAreaRequestFromMustBeDefined() {
+        AccessibilityRequestJson accessibilityRequestJson = AccessibilityRequestJson.builder()
+                .vehicle(VehicleCharacteristicsJson.builder()
+                        .emissionClass(EmissionClassJson.ZERO)
+                        .fuelTypes(List.of(FuelTypeJson.ELECTRIC))
+                        .build())
+                .area(BoundingBoxAreaRequestJson.builder().build())
+                .from(LocationJson.builder().build())
+                .build();
+
+        assertThat(accessibilityRequestValidator.verify(accessibilityRequestJson)).isTrue();
+    }
+
+    @Test
+    void verify_boundingBoxAreaRequestFromMustBeDefined_fromNotDefined() {
+        AccessibilityRequestJson accessibilityRequestJson = AccessibilityRequestJson.builder()
+                .vehicle(VehicleCharacteristicsJson.builder()
+                        .emissionClass(EmissionClassJson.ZERO)
+                        .fuelTypes(List.of(FuelTypeJson.ELECTRIC))
+                        .build())
+                .area(BoundingBoxAreaRequestJson.builder().build())
+                .build();
+
+        try {
+            accessibilityRequestValidator.verify(accessibilityRequestJson);
+        } catch (ApiException exception) {
+
+            assertThat(exception.getErrorId()).isEqualTo(UUID.fromString("43b85771-22ad-490b-95c8-e659fb3fc915"));
+            assertThat(exception.getStatusCode()).isEqualTo(HttpStatus.BAD_REQUEST);
+            assertThat(exception.getTitle()).isEqualTo("Invalid Request");
+            assertThat(exception.getDescription())
+                    .isEqualTo("When using a bounding box area request, the from parameter must also be set.");
+        }
+    }
+
 }
--- scripts/exampleRequests.sh
+++ scripts/exampleRequests.sh
@@ -1,5 +1,5 @@
 time curl -X 'POST' \
-  'https://data.ndw.nu/api/rest/static-road-data/accessibility-map/v2/accessibility.geojson' \
+  'http://localhost:8080/api/rest/static-road-data/accessibility-map/v2/accessibility.geojson' \
   -H 'accept: application/geo+json' \
   -H 'Accept-Encoding: gzip' \
   -H 'Content-Type: application/json' \
@@ -11,6 +11,57 @@ time curl -X 'POST' \
   "area": {
     "type": "municipality",
     "id": "GM0344"
+  },
+  "restrictions": [ {
+    "type": "roadSection",
+    "id": 269305008,
+    "direction": "forward",
+    "fraction": 0
+  }],
+  "vehicle": {
+    "type": "truck",
+    "width": 2,
+    "height": 2.5,
+    "weight": 20,
+    "length": 5.2,
+    "axleLoad": 4,
+    "hasTrailer": false,
+    "emissionClass": "euro_6",
+    "fuelTypes": [
+      "petrol"
+    ]
+  },
+  "exclusions": {
+    "emissionZoneTypes": [
+      "low_emission_zone"
+    ],
+    "emissionZoneIds": [
+      "NDW11_63a0104e-0b70-4b01-ad72-1ec692b41c47"
+    ]
+  }
+}'
+
+
+time curl -X 'POST' \
+  'http://localhost:8080/api/rest/static-road-data/accessibility-map/v2/accessibility.geojson' \
+  -H 'accept: application/geo+json' \
+  -H 'Accept-Encoding: gzip' \
+  -H 'Content-Type: application/json' \
+  -d '{
+  "destination": {
+    "latitude": 51.539914,
+    "longitude": 4.229669
+  },
+  "destination": {
+    "latitude": 51.537298,
+    "longitude": 4.244035
+  },
+  "area": {
+    "type": "boundingBox",
+    "minLatitude": 51.534549,
+    "maxLatitude": 51.541676,
+    "minLongitude": 4.224138,
+    "maxLongitude": 4.247336
   },
   "vehicle": {
     "type": "truck",
