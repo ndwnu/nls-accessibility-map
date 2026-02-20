cdb4d5ae101d705b9f21504009b2c51ce63c71de
Kevin Kamps <167181728+ndwlocatieservices@users.noreply.github.com>
2026-02-20 17:28:43 +0100

    [#109149] Fixed sonar remark.

--- accessibility/src/main/java/nu/ndw/nls/accessibilitymap/accessibility/reason/mapper/RestrictionMapper.java
+++ accessibility/src/main/java/nu/ndw/nls/accessibilitymap/accessibility/reason/mapper/RestrictionMapper.java
@@ -6,5 +6,6 @@ import nu.ndw.nls.accessibilitymap.accessibility.reason.dto.AccessibilityReason;
 
 public interface RestrictionMapper {
 
+    @SuppressWarnings("java:S1452")
     List<AccessibilityReason<?>> mapRestrictions(Restrictions restrictions);
 }
