3e37d7bd5aeadacc8a7ba11770a818cea999dcd2
NDW Locatie Services <167181728+ndwlocatieservices@users.noreply.github.com>
2026-02-24 15:35:47 +0100

    #107138  Add AKS-specific configuration, update volume claims, 10and refactor cache settings
    - Introduced `application-aks.yml` for AKS-specific settings.
    - Consolidated cache folders in pipeline configurations.
    - Adjusted persistent volume claim names and paths in Kubernetes templates.
    - Added dynamic `hostOauth` configuration in acceptance-test environments.
    - Included `deploy-and-run-infra-k8s.sh` script for streamlined Kubernetes deployments.

--- acceptance-test/component-test/src/test/java/nu/ndw/nls/accessibilitymap/jobs/test/component/driver/issue/IssueDriver.java
+++ acceptance-test/component-test/src/test/java/nu/ndw/nls/accessibilitymap/jobs/test/component/driver/issue/IssueDriver.java
@@ -9,10 +9,12 @@ import static com.github.tomakehurst.wiremock.client.WireMock.stubFor;
 import static com.github.tomakehurst.wiremock.client.WireMock.urlEqualTo;
 import static com.github.tomakehurst.wiremock.client.WireMock.urlPathMatching;
 import static com.github.tomakehurst.wiremock.client.WireMock.verify;
+import static nu.ndw.nls.accessibilitymap.jobs.test.component.driver.oauth.OAuthDriver.SIMULATED_BEARER_TOKEN;
 
 import java.util.Set;
 import lombok.RequiredArgsConstructor;
 import nu.ndw.nls.springboot.test.component.util.data.TestDataProvider;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 
@@ -26,7 +28,7 @@ public class IssueDriver {
 
         stubFor(post(urlPathMatching(
                 "/api/rest/static-road-data/location-data-issues/v1/issues"))
-              //  .withHeader(HttpHeaders.AUTHORIZATION, equalTo("Bearer %s".formatted(SIMULATED_BEARER_TOKEN)))
+                .withHeader(HttpHeaders.AUTHORIZATION, equalTo("Bearer %s".formatted(SIMULATED_BEARER_TOKEN)))
                 .willReturn(aResponse()
                         .withHeader("Content-Type", "application/json")
                         .withStatus(HttpStatus.ACCEPTED.value())
@@ -34,7 +36,7 @@ public class IssueDriver {
 
         stubFor(post(urlPathMatching(
                 "/api/rest/static-road-data/location-data-issues/v1/report/complete"))
-             //   .withHeader(HttpHeaders.AUTHORIZATION, equalTo("Bearer %s".formatted(SIMULATED_BEARER_TOKEN)))
+             .withHeader(HttpHeaders.AUTHORIZATION, equalTo("Bearer %s".formatted(SIMULATED_BEARER_TOKEN)))
                 .willReturn(aResponse()
                         .withHeader("Content-Type", "application/json")
                         .withStatus(HttpStatus.ACCEPTED.value())));
