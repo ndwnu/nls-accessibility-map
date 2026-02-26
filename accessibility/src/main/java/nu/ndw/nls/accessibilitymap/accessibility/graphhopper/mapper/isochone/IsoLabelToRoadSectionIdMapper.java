4b047b8d5aaad64de1cb5d38c393bd91fe4b2f7c
NDW Locatie Services <167181728+ndwlocatieservices@users.noreply.github.com>
2026-02-26 10:55:00 +0100

    [#108308] Fixed code style issues.

--- accessibility/src/main/java/nu/ndw/nls/accessibilitymap/accessibility/graphhopper/mapper/isochone/IsoLabelToRoadSectionIdMapper.java
+++ accessibility/src/main/java/nu/ndw/nls/accessibilitymap/accessibility/graphhopper/mapper/isochone/IsoLabelToRoadSectionIdMapper.java
@@ -4,23 +4,28 @@ import static nu.ndw.nls.routingmapmatcher.network.model.Link.REVERSED_LINK_ID;
 import static nu.ndw.nls.routingmapmatcher.network.model.Link.WAY_ID_KEY;
 
 import com.graphhopper.routing.util.EncodingManager;
+import com.graphhopper.storage.EdgeIteratorStateReverseExtractor;
 import com.graphhopper.util.EdgeIteratorState;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor
 public class IsoLabelToRoadSectionIdMapper {
+
+    private final EdgeIteratorStateReverseExtractor edgeIteratorStateReverseExtractor;
 
     public int map(
             EdgeIteratorState edge,
             EncodingManager encodingManager,
-            boolean isReversed,
             boolean isochroneCalculatedInReverse
     ) {
-        // Because edges in one direction have no reversed link id we need to check for that and then use the non reversed link id.
-        return (isReversed != isochroneCalculatedInReverse) && hasReversedLinkId(edge, encodingManager)
-                ? getReversedLinkId(edge, encodingManager)
-                : getLinkId(edge, encodingManager);
+        boolean isReversed = edgeIteratorStateReverseExtractor.hasReversed(edge);
 
+        // Because edges in one direction have no reversed link id we need to check for that and then use the non reversed link id.
+        return (isReversed == isochroneCalculatedInReverse) || !hasReversedLinkId(edge, encodingManager)
+                ? getLinkId(edge, encodingManager)
+                : getReversedLinkId(edge, encodingManager);
     }
 
     private boolean hasReversedLinkId(EdgeIteratorState edge, EncodingManager encodingManager) {
