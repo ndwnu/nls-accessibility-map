62052701189aa54bbca69778225080d93043f20f
NDW Locatie Services <167181728+ndwlocatieservices@users.noreply.github.com>
2026-04-24 11:07:21 +0200

    #112367 Refactor distributed lock handling to use `UUID` for `owner_id`, update related database schema, tests, and configuration. Simplify `JsonWriter` file creation logic and improve merge functionality in `NwbDataUpdates`.

--- accessibility/src/main/resources/db/changelog/changes/01_create_schema_and_lock_table.sql
+++ accessibility/src/main/resources/db/changelog/changes/01_create_schema_and_lock_table.sql
@@ -2,7 +2,7 @@ CREATE SCHEMA accessibility_map;
 CREATE TABLE accessibility_map.distributed_locks
 (
     lock_name   VARCHAR(255) PRIMARY KEY,
-    owner_id    VARCHAR(255),
+    owner_id uuid,
     acquired_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     lock_expiry TIMESTAMP
 );
