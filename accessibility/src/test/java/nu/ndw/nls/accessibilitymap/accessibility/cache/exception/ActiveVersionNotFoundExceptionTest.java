822559988a991070801f1a2376dbf5e4be0249e7
NDW Locatie Services <167181728+ndwlocatieservices@users.noreply.github.com>
2026-05-28 16:09:54 +0200

    #113962 Remove unused imports from `ActiveVersionNotFoundExceptionTest`.

--- accessibility/src/test/java/nu/ndw/nls/accessibilitymap/accessibility/cache/exception/ActiveVersionNotFoundExceptionTest.java
+++ accessibility/src/test/java/nu/ndw/nls/accessibilitymap/accessibility/cache/exception/ActiveVersionNotFoundExceptionTest.java
@@ -3,8 +3,6 @@ package nu.ndw.nls.accessibilitymap.accessibility.cache.exception;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.junit.jupiter.MockitoExtension;
 
 class ActiveVersionNotFoundExceptionTest {
 
