025c31a0884dbf7220b021c834b5c3dd1bd39b97
NDW Locatie Services <167181728+ndwlocatieservices@users.noreply.github.com>
2026-06-09 10:43:06 +0200

    [#116940] Fixed AccessibilityLinkCarMapper that did not respect one way directions.

--- accessibility/src/test/java/nu/ndw/nls/accessibilitymap/accessibility/graphhopper/mapper/AccessibilityLinkCarMapperTest.java
+++ accessibility/src/test/java/nu/ndw/nls/accessibilitymap/accessibility/graphhopper/mapper/AccessibilityLinkCarMapperTest.java
@@ -1,11 +1,14 @@
 package nu.ndw.nls.accessibilitymap.accessibility.graphhopper.mapper;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
 
 import nu.ndw.nls.accessibilitymap.accessibility.graphhopper.dto.AccessibilityLink;
 import nu.ndw.nls.routingmapmatcher.network.model.DirectionalDto;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
@@ -19,10 +22,17 @@ class AccessibilityLinkCarMapperTest {
     @InjectMocks
     private AccessibilityLinkCarMapper mapper;
 
-    @Test
-    void getAccessibility() {
+    @ParameterizedTest
+    @CsvSource({
+            "true, true",
+            "false, true",
+            "true, false",
+            "false, false"
+    })
+    void getAccessibility(boolean forwardDirection, boolean backwardDirection) {
 
-        assertThat(mapper.getAccessibility(link)).isEqualTo(new DirectionalDto<>(true, true));
+        when(link.getAccessibility()).thenReturn(new DirectionalDto<>(forwardDirection, backwardDirection));
+        assertThat(mapper.getAccessibility(link)).isEqualTo(new DirectionalDto<>(forwardDirection, backwardDirection));
     }
 
     @Test
