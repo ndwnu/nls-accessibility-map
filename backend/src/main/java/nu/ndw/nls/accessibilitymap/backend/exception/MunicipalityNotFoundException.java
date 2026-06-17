0bd198767a32dd75f690a4ac558c84ebf9b019e5
Kevin Kamps <167181728+ndwlocatieservices@users.noreply.github.com>
2026-06-17 16:36:38 +0200

    [#115756] Implemented generic api error handling.

--- backend/src/main/java/nu/ndw/nls/accessibilitymap/backend/exception/MunicipalityNotFoundException.java
+++ backend/src/main/java/nu/ndw/nls/accessibilitymap/backend/exception/MunicipalityNotFoundException.java
@@ -1,5 +1,7 @@
 package nu.ndw.nls.accessibilitymap.backend.exception;
 
+import nu.ndw.nls.springboot.web.error.exceptions.ResourceNotFoundException;
+
 public class MunicipalityNotFoundException extends ResourceNotFoundException {
 
     public MunicipalityNotFoundException(String message) {
