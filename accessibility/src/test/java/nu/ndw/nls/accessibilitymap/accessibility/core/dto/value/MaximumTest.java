a10b4854fc6856aab6bd295ec00144581649fb45
NDW Locatie Services <167181728+ndwlocatieservices@users.noreply.github.com>
2026-06-18 13:38:44 +0200

    #117373 Fixed MaximumTest

--- accessibility/src/test/java/nu/ndw/nls/accessibilitymap/accessibility/core/dto/value/MaximumTest.java
+++ accessibility/src/test/java/nu/ndw/nls/accessibilitymap/accessibility/core/dto/value/MaximumTest.java
@@ -43,28 +43,11 @@ class MaximumTest {
         assertThat(maximum.isExceeding(testValue, false)).isEqualTo(expectedResult);
     }
 
-    @ParameterizedTest
-    @CsvSource(textBlock = """
-            -1,   true
-            0,    true
-            1,    true
-            null, true
-            -1,   false
-            0,    false
-            1,    false
-            null, false
-            """, nullValues = "null")
-    void noMaximum(Double value, boolean inclusive) {
-        assertThat(Maximum.noMaximum().isExceeding(value, inclusive)).isFalse();
-    }
-
-    @ParameterizedTest
-    @CsvSource(textBlock = """
-            true
-            false
-            """)
-    void noMaximum_doubleMinMax(boolean inclusive) {
-        assertThat(Maximum.noMaximum().isExceeding(Double.MIN_VALUE, inclusive)).isFalse();
-        assertThat(Maximum.noMaximum().isExceeding(Double.MAX_VALUE, inclusive)).isFalse();
+    @Test
+    void noMaximum_shouldNeverBeExceeded() {
+        assertThat(Maximum.noMaximum().isExceeding(Double.MAX_VALUE, true)).isFalse();
+        assertThat(Maximum.noMaximum().isExceeding(Double.MAX_VALUE, false)).isFalse();
+        assertThat(Maximum.noMaximum().isExceeding(null, true)).isFalse();
+        assertThat(Maximum.noMaximum().isExceeding(null, false)).isFalse();
     }
 }
