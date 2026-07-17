0f880eae83b497e361d96abbe4e58ad8d9760ee0
Guido Senff <167181728+ndwlocatieservices@users.noreply.github.com>
2026-07-17 13:15:35 +0200

    #117641 Added enum values

--- accessibility/src/main/java/nu/ndw/nls/accessibilitymap/accessibility/core/dto/restriction/trafficsign/Category.java
+++ accessibility/src/main/java/nu/ndw/nls/accessibilitymap/accessibility/core/dto/restriction/trafficsign/Category.java
@@ -1,10 +1,17 @@
 package nu.ndw.nls.accessibilitymap.accessibility.core.dto.restriction.trafficsign;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
 public enum Category {
-    CHARGING,
-    LOADING,
-    PERMIT,
-    LOCAL_TRAFFIC,
-    DISABLED_TRANSPORT,
-    DANGEROUS_SUPPLIES
+    CHARGING("Charging"),
+    LOADING("Loading"),
+    PERMIT("Permit"),
+    LOCAL_TRAFFIC("Local traffic"),
+    DISABLED_TRANSPORT("Disabled transport"),
+    DANGEROUS_SUPPLIES("Dangerous supplies");
+
+    private final String value;
 }
