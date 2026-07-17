d87d138a941b1bfdaa22c5817f84b9942867e83e
NDW Locatie Services <167181728+ndwlocatieservices@users.noreply.github.com>
2026-07-17 13:15:35 +0200

    #117641 Added enum values

--- job/src/test/java/nu/ndw/nls/accessibilitymap/job/trafficsign/cache/mapper/CategoryMapperTest.java
+++ job/src/test/java/nu/ndw/nls/accessibilitymap/job/trafficsign/cache/mapper/CategoryMapperTest.java
@@ -1,9 +1,16 @@
 package nu.ndw.nls.accessibilitymap.job.trafficsign.cache.mapper;
 
-import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
+import static nu.ndw.nls.accessibilitymap.accessibility.core.dto.restriction.trafficsign.Category.CHARGING;
+import static nu.ndw.nls.accessibilitymap.accessibility.core.dto.restriction.trafficsign.Category.DANGEROUS_SUPPLIES;
+import static nu.ndw.nls.accessibilitymap.accessibility.core.dto.restriction.trafficsign.Category.DISABLED_TRANSPORT;
+import static nu.ndw.nls.accessibilitymap.accessibility.core.dto.restriction.trafficsign.Category.LOADING;
+import static nu.ndw.nls.accessibilitymap.accessibility.core.dto.restriction.trafficsign.Category.LOCAL_TRAFFIC;
+import static nu.ndw.nls.accessibilitymap.accessibility.core.dto.restriction.trafficsign.Category.PERMIT;
+import static org.assertj.core.api.Assertions.assertThat;
 
 import nu.ndw.nls.accessibilitymap.accessibility.core.dto.restriction.trafficsign.Category;
 import nu.ndw.nls.accessibilitymap.trafficsignclient.feign.generated.model.v1.ConditionPropertiesDtoV5Json.CategoryEnum;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
@@ -23,6 +30,17 @@ class CategoryMapperTest {
             """, nullValues = "null")
     void map(CategoryEnum sourceCategory, Category targetCategory) {
         assertThat(categoryMapper.map(sourceCategory)).isEqualTo(targetCategory);
+    }
+
+    @Test
+    void getValue() {
+        assertThat(CHARGING.getValue()).isEqualTo("Charging");
+        assertThat(LOADING.getValue()).isEqualTo("Loading");
+        assertThat(PERMIT.getValue()).isEqualTo("Permit");
+        assertThat(LOCAL_TRAFFIC.getValue()).isEqualTo("Local traffic");
+        assertThat(DISABLED_TRANSPORT.getValue()).isEqualTo("Disabled transport");
+        assertThat(DANGEROUS_SUPPLIES.getValue()).isEqualTo("Dangerous supplies");
+
     }
 
 }
