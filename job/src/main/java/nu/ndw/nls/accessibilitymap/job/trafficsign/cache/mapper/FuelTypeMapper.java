3ba0b2dc6f329bf07c620525a8abf2105d38efa4
NDW Locatie Services <167181728+ndwlocatieservices@users.noreply.github.com>
2026-07-20 15:40:22 +0200

    #117641 Now using the official 'onderborden'-codes when (de-)serializing values. Improved enum mappings with explicit mapping values in the test code, to improve refactoring options without breaking test code.

--- job/src/main/java/nu/ndw/nls/accessibilitymap/job/trafficsign/cache/mapper/FuelTypeMapper.java
+++ job/src/main/java/nu/ndw/nls/accessibilitymap/job/trafficsign/cache/mapper/FuelTypeMapper.java
@@ -8,6 +8,9 @@ public class FuelTypeMapper {
 
     @SuppressWarnings("java:S1172")
     public FuelType map(String fuelType) {
+        // Traffic API V5 defines fuelTypes as a String, but the values are currently never set and the enumerated values are not available.
+        // Returning null allows us to implement the fuel type logic without actually taking them into account. Not throwing an
+        // IllegalArgumentException to prevent our API from breaking when fuel types do become available.
         return null;
     }
 }
